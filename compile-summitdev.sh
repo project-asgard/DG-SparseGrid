d8ded6de1dd2d456fe64425d717520e43f875366
atj <adambsimpson@gmail.com>
2018-01-08 11:29:54 -0500

    Add CMakeLists.txt and Load cmake modules before running builder

--- compile-summitdev.sh
+++ compile-summitdev.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 module unload xalt
+module load cmake
 
 mkdir build
 cd build
--- compile-titan.sh
+++ compile-titan.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 module unload xalt
+module load cmake3
 
 mkdir build
 cd build
