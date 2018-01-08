193174a78a324b4e799d4e5202a8614fbef91b1e
atj <adambsimpson@gmail.com>
2018-01-08 12:05:06 -0500

    Make new directory on lustre for each CI job

--- tests-titan.sh
+++ tests-titan.sh
@@ -10,17 +10,18 @@ make
 
 # Save the current build directory and move to lustre
 BUILD_DIR=$(pwd)
-cd /lustre/atlas/scratch/atj/stf007
-
-# Assign a unique name to the executable based upon the GitLab CI job ID
-UNIQUE_EXE_NAME="DG_SparseGrid-${CI_JOB_ID}"
+RUN_DIR="/lustre/atlas/scratch/atj/stf007/DGSG-${CI_JOB_ID}"
+mkdir -p ${RUN_DIR}
+cd ${RUN_DIR}
 
 # Copy the executable to the lustre
-cp ${BUILD_DIR}/DG_SparseGrid ${UNIQUE_EXE_NAME}
+cp ${BUILD_DIR}/DG_SparseGrid ${RUN_DIR}
 
 # Create the aprun command to run the tests
 cat << EOF > aprun_command
-aprun -n 1 ${UNIQUE_EXE_NAME}
+#!/bin/bash -ex
+cd ${RUN_DIR}
+aprun -n 1 ./DG_SparseGrid
 EOF
 chmod +x ./aprun_command
 
