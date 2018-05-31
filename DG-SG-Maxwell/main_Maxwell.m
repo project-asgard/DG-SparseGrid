544a0f1932ba5f5ce6dfad7119f44bb473dded73
Mu, Lin <mul1@ornl.gov>
2018-05-31 09:48:10 -0400

    Update DG_SG_Maxwell.m

--- DG-SG-Maxwell/main_Maxwell.m
+++ DG-SG-Maxwell/main_Maxwell.m
@@ -30,23 +30,34 @@ dt = 1/10000;
 %*************************************************
 FMWT_COMP_x = OperatorTwoScale(Deg,2^Lev);
 
-%% Initial Conditions
+%*************************************************
+%% Step 1.2. Set Up Initial Conditions
 % F_1D: rhs coefficients
 % E_1D: Initial Condition for E
 % B_1D: Initial condition for B
+% Input:  Deg, Lev,Lmax, pde,FMWT_COMP_x
+% Output: F_1D,E_1D,B_1D
+%*************************************************
 [F_1D,E_1D,B_1D] = Intial_Con(Lev,Deg,Lmax,pde,FMWT_COMP_x);
 
-%% Coefficient Matrix for time-independent matrix
-GradX = Matrix_TI(Lev,Deg,Lmax,FMWT_COMP_x);
-
+%*************************************************
+%% Step 2. Hash Table and 1D Connectivity
+%*************************************************
+[Hash,InvHash]=HashTable(Lev,Dim);
 %1D connectivity
 Con1D=Connect1D(Lev);
-%% Hash Table
-% Sparse Grids Simulation
-[Hash,InvHash]=HashTable(Lev,Dim);
 
-%% Global Matrix for 3D Maxwell Eq.
+%*************************************************
+%% Step 3. Coefficient Matrix for Time-independent Matrix
+%*************************************************
+GradX = Matrix_TI(Lev,Deg,Lmax,FMWT_COMP_x);
+
+%*************************************************
+%% Step 4.3D Maxwell Solver
+%*************************************************
 % global rhs, E0, and B0 vectors
+% b_s is the RHS vector
+% E_s and B_s are used for error estimate
 [b_s,E_s,B_s]=GlobalRHS(Deg,F_1D,E_1D,B_1D,InvHash);
 
 %% Maxwell Solver
