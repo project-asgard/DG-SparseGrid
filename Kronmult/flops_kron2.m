a8478fbf3befabbb8471347a3ec428b52f876b0f
Ed D'Azevedo <dazevedoef@ornl.gov>
2018-06-18 12:35:36 -0400

    compute amount of temp storage

--- Kronmult/flops_kron2.m
+++ Kronmult/flops_kron2.m
@@ -1,10 +1,10 @@
-function [flops1, flops2, imethod] = flops_kron2( nrow1,ncol1, ...
+function [flops1, flops2, imethod, imem1, imem2] = flops_kron2( nrow1,ncol1, ...
                                                   nrow2,ncol2)
 
-% [flops1, flops2, imethod] = flops_kron2( nrow1,ncol1, ...
+% [flops1, flops2, imethod, imem1, imem2] = flops_kron2( nrow1,ncol1, ...
 %                                          nrow2,ncol2 )
 %
-% estimate flops for method 1 and method 2
+% estimate flops (and temporary storage) for method 1 and method 2
 %
 
 % ----------------------
@@ -18,6 +18,8 @@ function [flops1, flops2, imethod] = flops_kron2( nrow1,ncol1, ...
 % -------------------
 flops1 = 2.0*nrow2*ncol2 * ncol1;
 nrow_A2X = nrow2;
+ncol_A2X = nrow1;
+imem1 = nrow_A2X * ncol_A2X;
 flops1 = flops1 + 2.0 * nrow_A2X * ncol1 * nrow1 ;
 
 
@@ -30,6 +32,8 @@ nrow_X = ncol2;
 flops2 = 2.0 * nrow_X * ncol1 * nrow1;
 
 ncol_XA1t = nrow1;
+nrow_XA1t = nrow_X;
+imem2 = nrow_XA1t * ncol_XA1t;
 flops2 = flops2 + 2.0 * nrow2 * ncol2 * ncol_XA1t;
 
 if (flops1 <= flops2),
--- Kronmult/flops_kron3.m
+++ Kronmult/flops_kron3.m
@@ -1,26 +1,49 @@
-function [flops1, flops2, imethod] = flops_kron3( nrow1,ncol1, ...
+function [flops1, flops2, imethod,imem1,imem2] = flops_kron3( nrow1,ncol1, ...
                                                   nrow2,ncol2, ...
                                                   nrow3,ncol3)
 
-% [flops1, flops2, imethod] = flops_kron3( nrow1,ncol1, ...
+% [flops1, flops2, imethod,imem1,imem2] = flops_kron3( nrow1,ncol1, ...
 %                                          nrow2,ncol2, ...
 %                                          nrow3,ncol3)
 %
-% estimate flops for method 1 and method 2
+% estimate flops (and temporary storage) for method 1 and method 2
 %
+flops1 = 0;
+flops2 = 0;
+imem1 = 0;
+imem2 = 0;
 
 % ----------------------
 % method 1:
 % Ytmp = kron(A2,A3)*X
 % Y = Ytmp * transpose(A1)
 % ----------------------
-nrowX = ncol1*ncol2*ncol3;
-nvec = ncol1;
-flops1 = flops_kron2( nrow2,ncol2, nrow3,ncol3)*nvec;
-%
+
+% -----------------------------------
+% reshape X as (ncol2*ncol3) by ncol1;
+% -----------------------------------
+nrow_X = ncol2*ncol3;
+ncol_X = ncol1;
+
+% --------------------
+% Ytmp = kron(A2,A3)*X
+% --------------------
+nvec = ncol_X;
+[jflops1, jflops2, jmethod, jmem1, jmem2] = flops_kron2(nrow2,ncol2,nrow3,ncol3);
+if (jmethod == 1),
+  flops1 = flops1 +  jflops1 * nvec;
+  imem1 = imem1 + jmem1 * nvec;
+else
+  flops1 = flops1 + jflops2 * nvec;
+  imem1 = imem1 + jmem2 * nvec;
+end;
+% ------------------------------- 
 % Ytmp is (nrow2*nrow3) by ncol1;
-% 
+% ------------------------------- 
 nrow_Ytmp = (nrow2*nrow3);  
+ncol_Ytmp = ncol_X;
+
+imem1 = imem1 +  nrow_Ytmp * ncol_Ytmp;
 flops1 = flops1 + 2.0 * nrow_Ytmp * ncol1 * nrow1;
 
 
@@ -32,11 +55,34 @@ flops1 = flops1 + 2.0 * nrow_Ytmp * ncol1 * nrow1;
 
 % -------------------------------------
 % X reshaped as  (ncol2*ncol3) by ncol1
-% Ytmp is (ncol2*ncol3) by nrow1
 % -------------------------------------
-flops2 =  2.0 * (ncol2*ncol3) * ncol1 * nrow1;
+nrow_X = (ncol2*ncol3);
+ncol_X = ncol1;
 
-flops2 = flops2 + flops_kron2( nrow2,ncol2, nrow3,ncol3)*nrow1;
+% --------------------
+% Ytmp = X * transpose(A1)
+% so Ytmp is nrow_X by nrow1
+% --------------------
+nrow_Ytmp = nrow_X;
+ncol_Ytmp = nrow1;
+
+flops2 =  flops2 + 2.0 * nrow_X * ncol1 * nrow1;
+imem2 = imem2 + nrow_Ytmp * ncol_Ytmp;
+
+
+% --------------------
+% Y = kron(A2,A3)*Ytmp
+% --------------------
+nvec = ncol_Ytmp;
+[jflops1, jflops2, jmethod, jmem1, jmem2 ] = flops_kron2( nrow2,ncol2, nrow3,ncol3);
+if (jmethod == 1),
+  flops2 = flops2 + jflops1 * nvec;
+  imem2 = imem2 + jmem1 * nvec;
+else
+  flops2 = flops2 + jflops2 * nvec;
+  imem2 = imem2 + jmem2 * nvec;
+end;
+
 
 if (flops1 <= flops2),
   imethod = 1;
