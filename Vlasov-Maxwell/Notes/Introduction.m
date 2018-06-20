098658007495816c23c6563623e2bcb4238e93ba
Mu <lmm@lin-newmac.ornl.gov>
2018-06-20 18:40:01 -0400

    more html files

---
+++ Vlasov-Maxwell/Notes/Introduction.m
@@ -0,0 +1,14 @@
+%% Introduction of SG-DG
+%
+% SG-DG is a MATLAB software package for solving Vlasov Maxwell/Poisson 
+% equations
+%
+% * <NotesVM.html Introduction of Vlasov equations> 
+% * <WeakForms.html Numerical Schemes>
+% * <HashTable.html Algorithm 2 for HashTable>
+% * <TimeIndependentMatrices.html Algorithm 3 for Time-independent matrices>
+% * <TimeDependentMatrices.html Algorithm 4 for Time-dependent matrices>
+% * <GlobalVlasov.html Algorithm 6 for global Vlasov equation>
+% * <TimeAdvance.html Algorithm 7 for time advance method>
+% * <PoissonMaxwellSolver.html Algorithm 8 for Poisson/Maxwell solver>
+
