8c493bcc11c606f5d208d7bfbe1b54947b23f356
Mu <lmm@lin-newmac.ornl.gov>
2018-06-21 08:45:03 -0400

    rename notes

---
+++ Vlasov-Maxwell/Doc/HashTable/permn.m
@@ -0,0 +1,24 @@
+function [M, I] = permn(V, N)
+%========================================================
+% Compute all N combinators with repetition from vector V
+% PERMN - permutations with repetition
+%========================================================
+
+nV = numel(V) ;
+
+if nV==0 || N == 0
+    M = zeros(nV,N) ;
+    I = zeros(nV,N) ;
+    
+elseif N == 1
+    % return column vectors
+    M = V(:) ;
+    I = (1:nV).' ;
+else
+    % this is faster than the math trick used for the call with three
+    % arguments.
+    [Y{N:-1:1}] = ndgrid(1:nV) ;
+    I = reshape(cat(N+1,Y{:}),[],N) ;
+    M = V(I) ;
+end
+end
