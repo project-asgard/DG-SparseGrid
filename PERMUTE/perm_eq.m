2918644b044996c5569cc94c9bbe982bef58ed8d
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2018-07-02 10:22:42 -0400

    add option for ordering

--- PERMUTE/perm_eq.m
+++ PERMUTE/perm_eq.m
@@ -10,6 +10,7 @@ function [result] = perm_eq(idim,n)
 %           1,1; ...
 %           2,0]
 %
+use_1st_index_fastest = 1;
 
 if (idim == 1),
   result = [n];
@@ -19,8 +20,26 @@ end;
 if (idim == 2),
   ivec =  reshape(0:n,(n+1),1);
   result = zeros( n+1,2);
-  result(:,1) = ivec;
-  result(:,2) = n - ivec;
+  if (use_1st_index_fastest),
+    % ----------
+    % e.g. (0,3)
+    %      (1,2)
+    %      (2,1)
+    %      (3,0)
+    % ----------
+    result(:,1) = ivec;
+    result(:,2) = n - ivec;
+  else
+    % ----------
+    % e.g. (3,0)
+    %      (2,1)
+    %      (1,2)
+    %      (0,3)
+    % ----------
+    result(:,1) = n - ivec;
+    result(:,2) = ivec;
+  end;
+
   return;
 end;
 
@@ -28,9 +47,15 @@ icount = perm_eq_count(idim,n);
 result = zeros( icount, idim);
 ip = 1;
 for i=0:n,
-  isize = perm_eq_count( idim-1, i);
-  result( ip:(ip+isize-1), 1:(idim-1)) = perm_eq( idim-1,i);
-  result( ip:(ip+isize-1), idim ) = n-i;
+  if (use_1st_index_fastest), 
+    isize = perm_eq_count( idim-1, i);
+    result( ip:(ip+isize-1), 1:(idim-1)) = perm_eq( idim-1,i);
+    result( ip:(ip+isize-1), idim ) = n-i;
+  else
+    isize = perm_eq_count( idim-1, n-i);
+    result( ip:(ip+isize-1), 2:idim) = perm_eq(idim-1,n-i);
+    result( ip:(ip+isize-1), 1) = i;
+  end;
 
   ip = ip + isize;
 end;
