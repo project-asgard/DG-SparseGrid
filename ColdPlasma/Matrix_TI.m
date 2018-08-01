864ab114fc7502f68ff481eb60330c5651e87915
lin <mul1@ornl.gov>
2018-08-01 10:20:11 -0400

    updating

--- ColdPlasma/Matrix_TI.m
+++ ColdPlasma/Matrix_TI.m
@@ -16,13 +16,13 @@ format short e
 quad_num=10;
 %---------------
 
-alpha = 1000*Deg^3;
+alpha = 10*Deg^3;%*2^(2*Lev);
 isGenAencode = 0;
 
 
 % compute the trace values
 p_1 = legendre(-1,Deg);
-p_2 = legendre(1,Deg);
+p_2 = legendre( 1,Deg);
 
 [quad_x,quad_w]=lgwt(quad_num,-1,1);
 p_val = legendre(quad_x,Deg);
@@ -58,14 +58,16 @@ Ac = repmat({val},nx,1);
 GG = blkdiag(Ac{:});
 
 % Matrix for (u,v')
-val = Dp_val'*(quad_w*ones(1,Deg).*p_val)/hx;
-Ac = repmat({val},nx,1);
-G = blkdiag(Ac{:});
+% take derivative for test function v
+% val = Dp_val'*(quad_w*ones(1,Deg).*p_val)/hx;
+% Ac = repmat({val},nx,1);
+% G = blkdiag(Ac{:});
+
 
 % Matrix for (u',v)
 val = p_val'*(quad_w*ones(1,Deg).*Dp_val)/hx;
 Ac = repmat({val},nx,1);
-G2 = blkdiag(Ac{:});
+G = blkdiag(Ac{:});
 
 
 % Matrix for (u,v) = eye
@@ -91,9 +93,9 @@ Asub =  p_1'*p_2/2;
 Asup =  p_2'*(-p_1)/2;
 H = 1/hx*blktridiag([Amd],[Asub],[Asup],nx);
 % Correction for boundary
-H(1:Deg,1:Deg) = H(1:Deg,1:Deg)+(p_1'*(-p_1))/hx/2;
-H(end-Deg+[1:Deg],end-Deg+[1:Deg]) = H(end-Deg+[1:Deg],end-Deg+[1:Deg])+...
-                (p_2'*p_2)/hx/2;
+% H(1:Deg,1:Deg) = H(1:Deg,1:Deg)+(p_1'*(-p_1))/hx/2;
+% H(end-Deg+[1:Deg],end-Deg+[1:Deg]) = H(end-Deg+[1:Deg],end-Deg+[1:Deg])+...
+%                 (p_2'*p_2)/hx/2;
 
 %****************************************
 % Term for <{u_h'},[v]>ds
@@ -115,9 +117,9 @@ Asub = Dp_1'*p_2/2;
 Asup = Dp_2'*(-p_1)/2;
 J = 1/hx*blktridiag([Amd],[Asub],[Asup],nx)*2^(Lev+1);
 % Correction for boundary
-J(1:Deg,1:Deg) = J(1:Deg,1:Deg)+(Dp_1'*(-p_1))/hx*2^(Lev+1)/2;
-J(end-Deg+[1:Deg],end-Deg+[1:Deg]) = J(end-Deg+[1:Deg],end-Deg+[1:Deg])+...
-                (Dp_2'*p_2)/hx*2^(Lev+1)/2;
+% J(1:Deg,1:Deg) = J(1:Deg,1:Deg)+(Dp_1'*(-p_1))/hx*2^(Lev+1)/2;
+% J(end-Deg+[1:Deg],end-Deg+[1:Deg]) = J(end-Deg+[1:Deg],end-Deg+[1:Deg])+...
+%                 (Dp_2'*p_2)/hx*2^(Lev+1)/2;
 
 %****************************************
 % Term for <[u_h],[v]>ds
@@ -127,27 +129,132 @@ Asub = -p_1'*p_2;
 Asup =  p_2'*(-p_1);
 Q = 1/hx*blktridiag([Amd],[Asub],[Asup],nx)*2/hx;
 
+
+
 II = speye(dof_1D_x);
+
+% tic
+% For T1
+t1 = kron(kron(II,GG),II);
+t2 = kron(kron(II,II),GG);
+t3 = kron(kron(GG,II),II);
+tt = kron(G,G');
+t4 = -kron(tt,II);
+t5 = -kron(kron(G,II),G');
+t6 = -kron(II,tt);
+
 T1=[...
-    kron(kron(II,GG),II)+kron(kron(II,II),GG),-kron(kron(G,G'),II),-kron(kron(G,II),G');...
-    -kron(kron(G',G),II),kron(kron(GG,II),II)+kron(kron(II,II),GG),-kron(kron(II,G),G');...
-    -kron(kron(G',II),G),-kron(kron(II,G'),G),kron(kron(GG,II),II)+kron(kron(II,GG),II);...
+    t1+t2,t4,t5;...
+    t4',t2+t3,t6;...
+    t5',t6',t3+t1;...
     ];
+
+
+% For T2
+
+t1 = kron(kron(II,J),II);
+t2 = kron(kron(II,II),J);
+t3 = kron(kron(J,II),II);
+tt = kron(H,G');
+t4 = -kron(tt,II);
+t5 = -kron(kron(H,II),G');
+t6 = -kron(II,tt);
+tt = kron(G',H);
+t7 = -kron(tt,II);
+t8 = -kron(kron(G',II),H);
+t9 = -kron(II,tt);
+
+
 T2=[...
-    kron(kron(II,J),II)+kron(kron(II,II),J),-kron(kron(H,G'),II),-kron(kron(H,II),G');...
-    -kron(kron(G',H),II),kron(kron(J,II),II)+kron(kron(II,II),J),-kron(kron(II,H),G');...
-    -kron(kron(G',II),H),-kron(kron(II,G'),H),kron(kron(J,II),II)+kron(kron(II,J),II);...
+    t1+t2,t4,t5;...
+    t7,t2+t3,t6;...
+    t8,t9,t3+t1;...
     ];
+
+
+% For T3
+t1 = kron(kron(II,L),II);
+t2 = kron(kron(II,II),L);
+t3 = kron(kron(L,II),II);
+tt = kron(G,K);
+t4 = -kron(tt,II);
+t5 = -kron(kron(G,II),K);
+t6 = -kron(II,tt);
+tt = kron(K,G);
+t7 = -kron(tt,II);
+t8 = -kron(kron(K,II),G);
+t9 = -kron(II,tt);
+
+
 T3=[...
-    kron(kron(II,L),II)+kron(kron(II,II),L),-kron(kron(G,K),II),-kron(kron(G,II),K);...
-    -kron(kron(K,G),II),kron(kron(L,II),II)+kron(kron(II,II),L),-kron(kron(II,G),K);...
-    -kron(kron(K,II),G),-kron(kron(II,K),G),kron(kron(L,II),II)+kron(kron(II,L),II);...
+    t1+t2,t4,t5;...
+    t7,t2+t3,t6;...
+    t8,t9,t3+t1;...
     ];
-T4=[...
-    kron(kron(II,Q),II)+kron(kron(II,II),Q),zeros(dof_1D_x^3),zeros(dof_1D_x^3);...
-    zeros(dof_1D_x^3),kron(kron(Q,II),II)+kron(kron(II,II),Q),zeros(dof_1D_x^3);...
-    zeros(dof_1D_x^3),zeros(dof_1D_x^3),kron(kron(Q,II),II)+kron(kron(II,Q),II);...
-    ];
+
+
+% For T4
+t1 = kron(kron(II,Q),II);
+t2 = kron(kron(II,II),Q);
+t3 = kron(kron(Q,II),II);
+
+
+T4=blkdiag(t1+t2,t2+t3,t3+t1);
+clear t1 t2 t3 t4 t5 t6 t7 t8 t9 tt
+
+Mat = T1-T2-T3+alpha*T4-pde.w2*speye(dofs,dofs);
+figure;subplot(2,2,1);spy(T1);subplot(2,2,2);spy(T2);subplot(2,2,3);spy(T3);subplot(2,2,4);spy(T4);
+figure;spy(Mat);
+
+% % return
+% % 
+% % T1=[...
+% %     kron(kron(II,GG),II)+kron(kron(II,II),GG),-kron(kron(G,G'),II),-kron(kron(G,II),G');...
+% %     -kron(kron(G',G),II),kron(kron(GG,II),II)+kron(kron(II,II),GG),-kron(kron(II,G),G');...
+% %     -kron(kron(G',II),G),-kron(kron(II,G'),G),kron(kron(GG,II),II)+kron(kron(II,GG),II);...
+% %     ];
+% % 
+% % T2=[...
+% %     kron(kron(II,J),II)+kron(kron(II,II),J),-kron(kron(H,G'),II),-kron(kron(H,II),G');...
+% %     -kron(kron(G',H),II),kron(kron(J,II),II)+kron(kron(II,II),J),-kron(kron(II,H),G');...
+% %     -kron(kron(G',II),H),-kron(kron(II,G'),H),kron(kron(J,II),II)+kron(kron(II,J),II);...
+% %     ];
+% % T3=[...
+% %     kron(kron(II,L),II)+kron(kron(II,II),L),-kron(kron(G,K),II),-kron(kron(G,II),K);...
+% %     -kron(kron(K,G),II),kron(kron(L,II),II)+kron(kron(II,II),L),-kron(kron(II,G),K);...
+% %     -kron(kron(K,II),G),-kron(kron(II,K),G),kron(kron(L,II),II)+kron(kron(II,L),II);...
+% %     ];
+% % T4=[...
+% %     kron(kron(II,Q),II)+kron(kron(II,II),Q),zeros(dof_1D_x^3),zeros(dof_1D_x^3);...
+% %     zeros(dof_1D_x^3),kron(kron(Q,II),II)+kron(kron(II,II),Q),zeros(dof_1D_x^3);...
+% %     zeros(dof_1D_x^3),zeros(dof_1D_x^3),kron(kron(Q,II),II)+kron(kron(II,Q),II);...
+% %     ];
+
+
+% T1=[...
+%     kron(kron(II,GG),II)+kron(kron(II,II),GG),-kron(kron(G,G'),II),-kron(kron(G,II),G');...
+%     -kron(kron(G',G),II),kron(kron(GG,II),II)+kron(kron(II,II),GG),-kron(kron(II,G),G');...
+%     -kron(kron(G',II),G),-kron(kron(II,G'),G),kron(kron(GG,II),II)+kron(kron(II,GG),II);...
+%     ];
+% 
+% 
+% 
+% T2=[...
+%     kron(kron(II,J),II)+kron(kron(II,II),J),-kron(kron(H,G'),II),-kron(kron(H,II),G');...
+%     -kron(kron(G',H),II),kron(kron(J,II),II)+kron(kron(II,II),J),-kron(kron(II,H),G');...
+%     -kron(kron(G',II),H),-kron(kron(II,G'),H),kron(kron(J,II),II)+kron(kron(II,J),II);...
+%     ];
+% T3=[...
+%     kron(kron(II,L),II)+kron(kron(II,II),L),-kron(kron(G,K),II),-kron(kron(G,II),K);...
+%     -kron(kron(K,G),II),kron(kron(L,II),II)+kron(kron(II,II),L),-kron(kron(II,G),K);...
+%     -kron(kron(K,II),G),-kron(kron(II,K),G),kron(kron(L,II),II)+kron(kron(II,L),II);...
+%     ];
+% T4=[...
+%     kron(kron(II,Q),II)+kron(kron(II,II),Q),zeros(dof_1D_x^3),zeros(dof_1D_x^3);...
+%     zeros(dof_1D_x^3),kron(kron(Q,II),II)+kron(kron(II,II),Q),zeros(dof_1D_x^3);...
+%     zeros(dof_1D_x^3),zeros(dof_1D_x^3),kron(kron(Q,II),II)+kron(kron(II,Q),II);...
+%     ];
+% generate T1,T2,T3,and T4
 
 
 
@@ -356,15 +463,10 @@ if isGenAencode == 1
     A_encode{count}.A3=II;
     
 end
-% full(Q)
-% alpha
-% hx
 
-% size(T1)
-% dofs
-Mat = T1-T2-T3+alpha*T4-pde.w2*speye(dofs,dofs);
-figure;subplot(2,2,1);spy(T1);subplot(2,2,2);spy(T2);subplot(2,2,3);spy(T3);subplot(2,2,4);spy(T4);
-figure;spy(Mat);
+% Mat = T1-T2-T3+alpha*T4-pde.w2*speye(dofs,dofs);
+% figure;subplot(2,2,1);spy(T1);subplot(2,2,2);spy(T2);subplot(2,2,3);spy(T3);subplot(2,2,4);spy(T4);
+% figure;spy(Mat);
 
 % handling boundary condition
 
@@ -405,12 +507,11 @@ for i=0:2^Lev-1
 end
 
 ff =[kron(kron(f1x,f1y),f1z),kron(kron(f2x,f2y),f2z),kron(kron(f3x,f3y),f3z)]'*(2*pi^2-1);
-% size(ff)
-% size(Mat)
+
 sol = Mat\ff;
-% full([sol,ff/(2*pi^2-1)])
+
 figure;plot(sol-ff/(2*pi^2-1))
-% max(abs(sol-ff/(2*pi^2-1)))/norm(ff/(2*pi^2-1))
+
 uu = ff/(2*pi^2-1);
 [max(abs(sol-uu)) norm(sol-uu)]
 % compute the RHS term
