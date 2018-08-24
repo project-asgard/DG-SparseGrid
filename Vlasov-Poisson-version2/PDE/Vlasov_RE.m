9c41ba9110e0c01862e9d5662fd5a47532816797
Biewer, Adam <adam.biewer513@gmail.com>
2018-08-24 11:06:30 -0400

    Update Vlasov_RE.m

--- Vlasov-Poisson-version2/PDE/Vlasov_RE.m
+++ Vlasov-Poisson-version2/PDE/Vlasov_RE.m
@@ -385,25 +385,3 @@ function[vee]=re_vee(ne,Te,ve,coulombLog)
   vee = ne*phys.e^4*coulombLog/(4*pi*phys.eo^2*phys.me^2*ve.^3);
   %vee = 1/(3*pi^(1/2))*ne*(phys.e.^2 /(4*pi*phys.eo))^2*4*pi/sqrt(phys.me.*Te.^3)*coulombLog;
 endfunction
-
-function[a] = re_print_functions()
-  ne = 1e19;
-  Te = 10;
-  Zeff = 1.4;
-  v = 1;
-  ve = re_vThermal(Te)
-  x = re_x(v,ve)
-  Psi = re_Psi(x)
-  coulombLog = re_coulombLog(ne,Te)
-  Gamma = re_Gamma(ne,coulombLog)
-  delta = re_delta(ve)
-  vee = re_vee(ne,Te,ve,coulombLog)
-  Ec = re_Ec(ne,coulombLog)
-  lowerGamma = re_lowerGamma(v)
-  rho = re_rho(lowerGamma,v)
-  C_A = re_C_A(Gamma,v,Psi)
-  C_B = re_C_B(Zeff,Gamma,delta,v,x,Psi)
-  C_F = re_C_F(Te,Gamma,Psi)
-  a = 0;
-endfunction
-
