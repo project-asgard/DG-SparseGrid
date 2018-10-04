6d17c3e73ee764c823c56d5a5fb5ddfa5f5cf810
David Green <david.lindsay.green@gmail.com>
2018-10-04 12:22:21 -0400

    Cleaned up directory structure for reference branch

--- Kronmult/flops_kron1.m
+++
@@ -1,7 +0,0 @@
-function [flops1] = flops_kron1( nrow1, ncol1, nvec )
-% [flops1] = flops_kron1( nrow1, ncol1, nvec )
-%
-%  flops for  A1 * X
-%
-flops1 = 2.0*nrow1*ncol1*nvec;
-
--- MATLAB_2D_4D/kron_mult3.m
+++
@@ -1,9 +0,0 @@
-function Y = kron_mult3(A1,A2,A3,X)
-% Y = kron_mult3(A1,A2,A3,X)
-%
-Acell{1} = A1;
-Acell{2} = A2;
-Acell{3} = A3;
-nkron = 3;
-Y = kron_multd(nkron,Acell,X);
-
