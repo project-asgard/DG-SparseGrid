92677bbcf558190f2a1e5e6912479ace10270358
Mu <lmm@lin-newmac.ornl.gov>
2018-10-04 15:32:24 -0400

    fixed bugs of all cell

--- Vlasov-Poisson-version2/PDE/Vlasov4.m
+++ Vlasov-Poisson-version2/PDE/Vlasov4.m
@@ -4,7 +4,7 @@ function pde=Vlasov4
 
 % Parameters
 pde.Dim = 2;
-pde.Lev = [3 3];
+pde.Lev = [4 4];
 pde.domain = [0 20*pi/3; -13 13]';
 
 k_0=0.3;
--- Vlasov-Poisson-version2/PERMUTE/AllCell.m
+++ Vlasov-Poisson-version2/PERMUTE/AllCell.m
@@ -7,21 +7,39 @@ function cell = AllCell(Lev)
 %===================================================
 dim = length(Lev);
 
-nMax = zeros(1,dim);
-for ii =1:dim
-    nMax(ii) = max(0,2^max(0,Lev(ii)-1)-1);
-end
-nz = prod(nMax+1);
-    
+% nMax = zeros(1,dim);
+% for ii =1:dim
+%     nMax(ii) = max(0,2^max(0,Lev(ii)-1)-1);
+% end
+% nz = prod(nMax+1);
+%     
+% 
+% cell = zeros(nz,dim);
 
-cell = zeros(nz,dim);
-for ii=1:dim
-    % Both of the following two methods can work
-    % Method 1
-    % tmp=repmat([0:nMax(ii)]',nz/(nMax(ii)+1),1);
-    % cell(:,ii)=tmp(:);
-    % Method 2
-    cell(:,ii)=repmat([0:nMax(ii)]',nz/(nMax(ii)+1),1);
+% for ii=1:dim
+%     % Both of the following two methods can work
+%     % Method 1
+%     % tmp=repmat([0:nMax(ii)]',nz/(nMax(ii)+1),1);
+%     % cell(:,ii)=tmp(:);
+%     % Method 2
+% 
+%     cell(:,ii)=repmat([0:nMax(ii)]',nz/(nMax(ii)+1),1);
+% 
+% end
+
+
+for i = 1:dim
+    nMax = max(0,2^max(0,Lev(i)-1)-1);
+    if nMax == 0
+        value{i} = 0;
+    else
+    value{i}=[0:nMax];
+    end
 end
+cell = allcomb(value{:});
+
+
+
+
 
 end
