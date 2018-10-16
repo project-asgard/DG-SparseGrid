fca1cfbae1ea39cfca6fe7de04b7da4261c4cc41
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2018-10-16 14:12:02 -0400

    option to compute inline flops1, flops2

--- Kronmult/kronmult2.m
+++ Kronmult/kronmult2.m
@@ -1,12 +1,11 @@
 function Y = kronmult2(A1,A2, X )
 % Y = kronmult2(A1,A2, X )
-global idebug;
 
-nrow1 = size(A1,1);
-ncol1 = size(A1,2);
+idebug = 0;
 
-nrow2 = size(A2,1);
-ncol2 = size(A2,2);
+[nrow1,ncol1] = size(A1);
+[nrow2,ncol2] = size(A2);
+
 
 nrowX = ncol1*ncol2;
 
@@ -22,7 +21,33 @@ nvec = numel(X)/nrowX;
 nrowY = nrow1*nrow2;
 Y = zeros( nrowY, nvec );
 
-[flops1,flops2,imethod] = flops_kron2( nrow1,ncol1, nrow2,ncol2);
+use_flops_kron2 = 0;
+if (use_flops_kron2),
+  [flops1,flops2,imethod] = flops_kron2( nrow1,ncol1, nrow2,ncol2);
+else
+  flops1 = 2*(nrow2 * ncol1)*(ncol2 + nrow1);
+  flops2 = 2*(nrow2 + ncol1)*(ncol2 * nrow1);
+  if (flops1 <= flops2),
+	  imethod  = 1;
+  else
+	  imethod = 2;
+  end;
+  if (idebug >= 1),
+     % ------------
+     % double check
+     % ------------
+     [c_flops1,c_flops2,c_imethod] = ...
+	flops_kron2(nrow1,ncol1,  nrow2,ncol2);
+     isok = (c_flops1 == flops1) & ...
+            (c_flops2 == flops2) & ...
+	    (c_imethod == imethod);
+     if (~isok),
+	error(sprintf('cflops1=%g,flops1=%g,cflops2=%g,flops2=%g,c_imethod=%g,imethod=%g', ...
+	              cflops1,flops1, cflops2,flops2, c_imethod,imethod));
+        return;
+     end;
+
+end;
 
 if (idebug >= 1),
   disp(sprintf('kronmult2: flops1=%g, flops2=%g, imethod=%d', ...
