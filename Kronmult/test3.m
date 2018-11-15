9a70181c13ab721620c12d253117c7dd627b6d62
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2018-11-15 15:28:21 -0500

    checkin kron6

--- Kronmult/test3.m
+++ Kronmult/test3.m
@@ -7,6 +7,7 @@ A2 = rand(3,4);
 A3 = rand(4,5);
 A4 = rand(5,6);
 A5 = rand(6,7);
+A6 = rand(3,2);
 
 nrow1 = size(A1,1);
 ncol1 = size(A1,2);
@@ -22,6 +23,9 @@ ncol4 = size(A4,2);
 
 nrow5 = size(A5,1);
 ncol5 = size(A5,2);
+
+nrow6 = size(A6,1);
+ncol6 = size(A6,2);
 
 % ------------
 [flops1,flops2,imethod] = flops_kron2( nrow1,ncol1, ...
@@ -76,5 +80,15 @@ disp(sprintf('(A2)=(%d,%d),(A3)=(%d,%d),(A4)=(%d,%d),(A5)=(%d,%d),flops1=%g,flop
                                        nrow5,ncol5 );
 disp(sprintf('(A1)=(%d,%d),(A2)=(%d,%d),(A3)=(%d,%d),(A4)=(%d,%d),(A5)=(%d,%d),flops1=%g,flops2=%g', ...
     nrow1,ncol1,  nrow2,ncol2,  nrow3,ncol3, nrow4,ncol4, nrow5,ncol5, flops1, flops2 ));
+
+% ------------
+[flops1,flops2,imethod] = flops_kron6( nrow1,ncol1, ...
+                                       nrow2,ncol2, ...
+                                       nrow3,ncol3, ...
+                                       nrow4,ncol4, ...
+                                       nrow5,ncol5, ...
+                                       nrow6,ncol6 );
+disp(sprintf('(A1)=(%d,%d),(A2)=(%d,%d),(A3)=(%d,%d),(A4)=(%d,%d),(A5)=(%d,%d),(A6)=(%d,%d),flops1=%g,flops2=%g', ...
+    nrow1,ncol1,  nrow2,ncol2,  nrow3,ncol3, nrow4,ncol4, nrow5,ncol5, nrow6,ncol6, flops1, flops2 ));
 
 
