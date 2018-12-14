7762d9db5015f00bc1fcde265534a4d13b7268b7
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2018-12-14 11:49:10 -0500

    checkin general kron_split

--- Kronmult/kron_split2.m
+++ Kronmult/kron_split2.m
@@ -1,8 +1,15 @@
 function [iperm] = kron_split2( nA, nB1, nB2 )
-%
+% ---------------------------------------------------
 % [iperm] = kron_split2( nA, nB1, nB2 )
-% kron(A, [B1,B2]) * P = [ kron(A,B1) , kron(A,B2) ]
+% kron(A, [B1,B2]) * P equals [ kron(A,B1) , kron(A,B2) ]
 %
+% similarly
+% P * kron(A, [B1;
+%              B2])
+% equals
+% [ kron(A,B1); 
+%   kron(A,B2)]
+% ---------------------------------------------------
 nB = nB1 + nB2;
 ip = reshape( 1:(nB*nA), [nB,nA]);
 %
@@ -20,6 +27,9 @@ ip2 = ip( (nB1+1):nB, 1:nA);
 % generate new permutation vector
 % -------------------------------
 iperm = zeros( nB*nA,1);
-iperm(1:(nB1*nA)) = reshape( ip1, (nB1*nA),1);
-iperm( (nB1*nA) + (1:(nB2*nA)) ) = reshape( ip2, (nB2*nA),1);
+iperm(1:(nB1*nA)) = reshape( ip1, numel(ip1),1);
+
+i1 = (nB1*nA) + 1;
+i2 = (nB1*nA) + (nB2*nA);
+iperm( i1:i2 ) = reshape( ip2, numel(ip2),1);
 
