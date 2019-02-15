73d043114fbdd16a9067eb7158c2c9206ce7a491
Timothy Younkin <tyounkin@Timothys-MacBook-Pro.local>
2019-02-15 15:10:06 -0500

    Corrected Quadrature/lgwt.m algorithm based on recurrence relation

--- Quadrature/lgwt.m
+++ Quadrature/lgwt.m
@@ -35,18 +35,14 @@ y0=2;
 % Iterate until new points are uniformly within epsilon of old points
 while max(abs(y-y0))>eps
     
-    
     L(:,1)=1;
-    Lp(:,1)=0;
-    
     L(:,2)=y;
-    Lp(:,2)=1;
     
     for k=2:N1
         L(:,k+1)=( (2*k-1)*y.*L(:,k)-(k-1)*L(:,k-1) )/k;
     end
  
-    Lp=(N2)*( L(:,N1)-y.*L(:,N2) )./(1-y.^2);   
+    Lp=(N1)*( L(:,N1)-y.*L(:,N2) )./(1-y.^2);   
     
     y0=y;
     y=y0-L(:,N2)./Lp;
@@ -57,7 +53,7 @@ end
 x=(a*(1-y)+b*(1+y))/2;      
 
 % Compute the weights
-w=(b-a)./((1-y.^2).*Lp.^2)*(N2/N1)^2;
+w=(b-a)./((1-y.^2).*Lp.^2);
 
 x=x(end:-1:1);
 w=w(end:-1:1);
