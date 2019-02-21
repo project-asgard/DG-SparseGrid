8bd7ebff17108579ce11a942e0257bfcc97b8cf0
lmm <mul1@ornl.gov>
2019-02-21 16:32:51 -0500

    update plotting file

---
+++ PlotFile.m
@@ -0,0 +1,9 @@
+load('Data_Plot.mat')
+figure;
+subplot(1,2,1)
+mesh(x_2D_plot,y_2D_plot,FucVal);
+
+
+[f_nd_t] = converttoRealSpace(2,lev,deg,'SG',[0,0],[1,1],F0,6);
+subplot(1,2,1)
+mesh(reshape(f_nd_t,64,64))
