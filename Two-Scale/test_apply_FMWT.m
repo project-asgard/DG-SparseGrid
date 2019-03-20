68cf345da91a8309366eb9fd467e7c5166b21682
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2019-03-20 16:32:05 -0400

    option to print out gflop rate

--- Two-Scale/test_apply_FMWT.m
+++ Two-Scale/test_apply_FMWT.m
@@ -1,5 +1,7 @@
 % simple script to test apply_FMWT
 %
+is_show_gflops = 0;
+
 kdeg = 10;
 Lev = 8;
 n = kdeg * 2^Lev;
@@ -11,6 +13,13 @@ FMWT =  OperatorTwoScale(kdeg,2^Lev);
 time_fmwt = toc();
 disp(sprintf('kdeg=%d,Lev=%d,n=%d, time for OperatorTwoScale is %g',...
               kdeg,   Lev,   n,  time_fmwt));
+FMWT = full(FMWT);
+% ------------------------------
+% generate sparse matrix version
+% ------------------------------
+[ii,jj,aa] = find( FMWT );
+FMWTs = sparse(ii,jj,aa,n,n);
+clear ii jj aa;
 
 imethod = 1;
 tic;
@@ -28,16 +37,30 @@ relerr = err / max( norm(Y_1,1), norm(Y_2,1) );
 disp(sprintf('kdeg=%d,Lev=%d,err=%g,relerr=%g, time_1=%g, time_2=%g', ...
               kdeg,   Lev,   err,   relerr,    time_1,    time_2 ));
 
+if (is_show_gflops),
+  gflops = 2.0*n*n * size(X,2) * 1e-9/time_1;
+  disp(sprintf('Dense Gflops is %g Gflops/sec ', ...
+                gflops ));
+  
+  
+  gflops = 2.0*nnz(FMWTs) * size(X,2) * 1e-9/time_2;
+  disp(sprintf('Structure Dense Gflops is %g Gflops/sec ', ...
+                gflops ));
+end;
+
 % --------------------
 % test sparse version
 % --------------------
-[ii,jj,aa] = find( FMWT );
-FMWTs = sparse(ii,jj,aa,n,n);
-clear ii jj aa;
 
 tic;
 Y_s = FMWTs * X;
 time_s = toc;
 disp(sprintf('nnz(FMWTs)=%g, time_s=%g', ...
               nnz(FMWTs),    time_s ));
+
+if (is_show_gflops),
+  gflops = 2.0*nnz(FMWTs) * 1e-9 / time_s;
+  disp(sprintf('sparse Gflops rate is %g Gflops/sec', ...
+                gflops ));
+end;
 
