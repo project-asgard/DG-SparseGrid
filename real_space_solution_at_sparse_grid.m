42f9d2121f11fb41920c12d1c92d30f6384bc50a
David Green <david.lindsay.green@gmail.com>
2019-05-28 12:29:50 -0400

    Updated to use 1D coords, not global

--- real_space_solution_at_sparse_grid.m
+++ real_space_solution_at_sparse_grid.m
@@ -27,8 +27,8 @@ fval_r = zeros(nPts,1);
 % Loop over elements, and evaluate which other elements each contributes to
 % and evaluate and add its contribution.
 
-for elem=1:nElem    
-      
+for elem=1:nElem
+    
     %%
     % Get this element coordinate vector
     
@@ -36,34 +36,33 @@ for elem=1:nElem
     
     [myCoord,myCoordL,myCoordR] = getMyRealSpaceCoord(pde,idx);
     lev_D = pde.elements.lev_p1(idx,:)-1;
-    
-    %%
-    % Get this element coefficients
-    
-    elementDOF = deg^nDims;
-    i1 = (elem-1)*elementDOF+1;
-    i2 = elem*elementDOF;
+    pos_D = pde.elements.pos_p1(idx,:)-1;
     
     clear kronMatList;
     for d=1:nDims
-        
+             
         dim = pde.dimensions{d};
+        lev = lev_D(d);
+        pos = pos_D(d);
         
         %%
         % Get this elements piece of 1D FMWT (legendre -> wavelet)
         
-        FMWT = dim.FMWT;       
+        idx_1D = lev_cell_to_singleD_index(lev,pos);
+        i1 = (idx_1D-1)*deg+1;
+        i2 = idx_1D*deg;
+        
+        FMWT = dim.FMWT;
         F_T = FMWT(i1:i2,i1:i2)'; % note the transpose
-
+        
         %%
         % Get this elements scale factor
         
-        lev = lev_D(d);
         dMin = dim.domainMin;
         dMax = dim.domainMax;
         h = (dMax-dMin)/(2^lev);
         scale_fac = sqrt(1/h);
-                
+        
         %%
         % Map desired coordinates to this elements 1D [-1,+1] range
         
@@ -81,18 +80,18 @@ for elem=1:nElem
         %%
         % F_T = wavelet  -> legendre
         % M   = legendre -> real
-          
+        
         kron_mat_list{d} = M * F_T; % Build kron list
         
     end
     
     fval_r = fval_r + kron_multd(nDims,kron_mat_list,wavelet_coeffs);
-  
-%     figure(99);
-%     hold on
-%     x = allCoords(:,1);
-%     [X,I] = sort(x);
-%     plot(X,fval_r(I),'-o')
+    
+    %     figure(99);
+    %     hold on
+    %     x = allCoords(:,1);
+    %     [X,I] = sort(x);
+    %     plot(X,fval_r(I),'-o')
     
 end
 
