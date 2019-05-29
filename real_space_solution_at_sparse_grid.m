de6a56144d490b77193f4379fc65e44d081767dd
David Green <david.lindsay.green@gmail.com>
2019-05-29 16:12:34 -0400

    sending to Lin for help

--- real_space_solution_at_sparse_grid.m
+++ real_space_solution_at_sparse_grid.m
@@ -2,7 +2,10 @@ function fval_r = real_space_solution_at_sparse_grid(pde,wavelet_coeffs)
 
 nElem = numel(pde.elementsIDX);
 nDims = numel(pde.dimensions);
+nDOF = numel(wavelet_coeffs);
 deg = pde.dimensions{1}.deg;
+
+assert(nDOF == nElem*deg^nDims);
 
 %%
 % Get center coordinates for all elements
@@ -17,7 +20,7 @@ deg = pde.dimensions{1}.deg;
 % For now just use a simple, ordered set of coordinates (the above would
 % give the centers of all the elements)
 
-allCoords(:,1) = [-1:0.01:+1];
+allCoords(:,1) = [-1:0.005:+1];
 
 nPts = numel(allCoords(:,1));
 
@@ -29,33 +32,32 @@ fval_r = zeros(nPts,1);
 %
 % M * f_legendre => f_realspace
 
+
+MM = sparse(nPts,nDOF);
 for d=1:nDims
     
-    dim = pde.dimensions{d};
+    dim         = pde.dimensions{d};
+    max_lev      = dim.lev;
+    nLegendre   = 2^max_lev;
     
-    clear M;
-    for elem=1:nElem
-        
-        %%
-        % Get the lev, pos and range of this element
-        
-        idx = pde.elementsIDX(elem);
-        [elem_coord_D,elem_coord_L_D,elem_coord_R_D] = getMyRealSpaceCoord(pde,idx);
-        lev_D = pde.elements.lev_p1(idx,:)-1;
-        lev = lev_D(d);
-        
-        %%
-        % Get the scale_fac for this element
-        
-        xMin = elem_coord_L_D(d); % left side of element
-        xMax = elem_coord_R_D(d); % right side of element
-        h = (xMax-xMin)/(2^lev);
-        scale_fac = sqrt(1/h);
+    %%
+    % Get the scale_fac for this level
+    
+    dMin = dim.domainMin;
+    dMax = dim.domainMax;
+    h = (dMax-dMin)/nLegendre;
+    scale_fac = sqrt(1/h);
+    
+    x = allCoords(:,d);
+    
+    clear MM;
+    for l=1:nLegendre
         
         %%
         % Get the normalized to [-1,+1] coordinates for this element
         
-        x = allCoords(:,d);
+        xMin = dMin + (l-1)*h;
+        xMax = xMin + h;
         x_norm = (x - xMin)/(xMax-xMin)*2-1;
         
         %%
@@ -64,25 +66,34 @@ for d=1:nDims
         
         p_val = lin_legendre(x_norm,deg) * scale_fac;
         
-        %         %%
-        %         % Plot realspace basis functions
-        %
-        %         figure(88)
-        %         cla
-        %         for dd=1:deg
-        %             plot(x_norm,p_val(:,dd));
-        %             hold on
-        %         end
-        %         hold off
+        %%
+        % Get the lev, pos and range of this element
         
-        i1 = deg*(elem-1)+1;
-        i2 = deg*(elem);
+        %         idx = pde.elementsIDX(l);
+        %         [elem_coord_D,elem_coord_L_D,elem_coord_R_D] = getMyRealSpaceCoord(pde,idx);
+        %         lev_D = pde.elements.lev_p1(idx,:)-1;
+        %         lev = lev_D(d);
         
-        M(:,i1:i2) = p_val;
+        %%
+        % Plot realspace basis functions
+        
+        figure(88)
+%         cla
+        for dd=1:deg
+            plot(x,1./(1./(p_val(:,dd))),'-o');
+            hold on
+            ylim([-10,10]);
+        end
+%         hold off
+        
+        j1 = deg*(l-1)+1;
+        j2 = deg*(l);
+        
+        MM(:,j1:j2) = p_val;
         
     end
     
-    M_D{d} = M;
+    M_D{d} = MM;
     
 end
 
@@ -99,6 +110,12 @@ for elem=1:nElem
     clear kronMatList;
     for d=1:nDims
         
+%         IDlev = pde.elements.lev_p1(pde.elementsIDX(elem),d)-1;
+%         IDpos = pde.elements.pos_p1(pde.elementsIDX(elem),d)-1;
+%         IDe = lev_cell_to_singleD_index(IDlev,IDpos);
+%         
+%         assert(IDe==elem);
+        
         i1 = deg*(elem-1)+1;
         i2 = deg*(elem);
         
@@ -113,17 +130,18 @@ for elem=1:nElem
     
     fval_r =  fval_r + kron_multd(nDims,kron_mat_list,wavelet_coeffs);
     
-%     figure(99);
-%     hold on
-%     x = allCoords(:,1);
-%     [X,I] = sort(x);
-%     plot(X,fval_r(I),'-o')
-   
+    %     figure(99);
+    %     hold on
+    %     x = allCoords(:,1);
+    %     [X,I] = sort(x);
+    %     plot(X,fval_r(I),'-o')
+    
 end
 
 %%
 % Test plot
 
+figure()
 x = allCoords(:,1);
 [X,I] = sort(x);
 plot(X,fval_r(I),'o')
