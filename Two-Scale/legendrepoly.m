c2b6d781c711691c525e544c458dfcf1fc252e13
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2019-06-14 16:25:01 -0400

    add option to renormalize function

--- Two-Scale/legendrepoly.m
+++ Two-Scale/legendrepoly.m
@@ -1,10 +1,15 @@
-function F = legendrepoly( ndeg, x )
+function F = legendrepoly( ndeg, x, do_normalize_in )
 % function F = legendrepoly( ndeg, x )
 % evaluate orthogonal legendre polynomial of degree 0, 1, ..., ndeg
 % at positions x(1), x(2), ... x(n)
 % F(i,j) = P_{j-1}( x(i) ),   
 % where P_j(x) is legendre polynomial of degree j
 %
+do_normalize = 0;
+if (nargin >= 3),
+   do_normalize = do_normalize_in;
+end;
+
 n = numel(x);
 x = reshape(x,n,1);
 F = zeros( n,ndeg+1);
@@ -24,6 +29,18 @@ for L=1:ndeg-1,
 
       PLp1 = ((2*L+1)*x(1:n).*PL(1:n) - L*PLm1(1:n))/(L+1);
       F(1:n,ioff+ (L+1) )  = PLp1(1:n);
+end;
+
+if (do_normalize),
+% ----------------------------------------------------
+% integral( P_n(x) * P_n(x), over x=-1..1 ) = 2/(2*n +1)
+% thus rescale Phat_n(x) =  P_n(x)/sqrt( 2/(2*n+1) )
+% integral( Phat_n(x) * Phat_n(x), over x=-1..1 ) = 1
+% ----------------------------------------------------
+for L=0:ndeg,
+   dnorm = 2/(2*L+1);
+   dscale = sqrt( dnorm );
+   F(1:n, ioff+L) = F(1:n,ioff+L)/dscale;
 end;
 
 end
