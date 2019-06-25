858d9a13813e1ab878309323d9f5b928bcb8295b
Mu, Lin <mul1@ornl.gov>
2019-06-25 15:57:40 -0400

    Update comments for PoissonSolve.m

--- PoissonSolve.m
+++ PoissonSolve.m
@@ -19,12 +19,28 @@ b_poisson=sparse(2*dof_1D_x,1);
 %-----------------------------------------------------
 % Phi_v(0,0) corresponding to the
 
-Index=[1:k]'+[0:k^2:(2^(Lev_x)-1)*k^2];
+% f is the given coeffients of the wavelet basis
+% rho = int_v f dv: 
+% we use the orthogonal property (1,w_j) = 0 for the j>1
+%       and (1,w_1) = sqrt(2*Vmax)
+% RHS = int_x (1-rho)*q dx 
+% by orthogonal property, only the first basis gives nonzero RHS, which is
+%       sqrt(Lmax)
+
+
+Index=[1:k]'+[0:k^2:(2^(Lev_x)-1)*k^2]; 
+% take out all the coefs corresponding to the first basis corresponding to v
+% compute (1-rho,q)
 tmp_b=[sqrt(Lmax); zeros(dof_1D_x-1,1)]...
       -sqrt(2*Vmax)*f(Index(:));%f(1:dof_1D_x);
 
-
-b_poisson(dof_1D_x+1:end)=(FMWT_COMP_x(:,2:end-1)*FMWT_COMP_x(:,2:end-1)')*tmp_b;
+% Boundary condition is enforced as phi(0) = 0; phi(Lmax) = 0
+% 1. Convert back to Lengendre basis: C1 = FMWT_COMP_x(:,2:end-1)')*tmp_b
+% We exclude first and last basis, and assume the coefs of Lengendre basis are zeros
+% 2. Convert to wavelet basis: C2 = FMWT_COMP_x(:,2:end-1)*C1
+%b_poisson(dof_1D_x+1:end)=(FMWT_COMP_x(:,2:end-1)*FMWT_COMP_x(:,2:end-1)')*tmp_b;
+% Suggested by Ed, the above is changed to
+b_poisson(dof_1D_x+1:end)=FMWT_COMP_x(:,2:end-1)*(FMWT_COMP_x(:,2:end-1)'*tmp_b);
 
 
 x_poisson=DeltaX\b_poisson;
