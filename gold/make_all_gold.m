244d6cc5e058fe56711539ecc7073f65892099af
David Green <david.lindsay.green@gmail.com>
2019-06-27 00:52:52 -0400

    Adding gold testing data for time_advance component

--- gold/make_all_gold.m
+++ gold/make_all_gold.m
@@ -1,8 +1,26 @@
+disp('Generating gold data for connectivity component');
 gold_connect
+
+disp('Generating gold data for hash table component');
 gold_hash_table
+
+disp('Generating gold data for matlab component');
 gold_matlab
+
+disp('Generating gold data for quadrature component');
 gold_quadrature
+
+disp('Generating gold data for transformations component');
 gold_transformations
+
+disp('Generating gold data for pde component');
 gold_pde
+
+disp('Generating gold data for coefficients component');
 gold_coefficients
+
+disp('Generating gold data for batch component');
 gold_batch
+
+disp('Generating gold data for time_advance component');
+gold_time_advance
