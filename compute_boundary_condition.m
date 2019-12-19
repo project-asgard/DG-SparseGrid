16bb520e5500d6398b52bfb5e5a2eada6a7ce2e9
Louis Wonnell <lwonnell@ksu.edu>
2019-12-19 16:05:40 -0500

    Modified compute_boundary_condition.m to move away from xMin or xMax if g_func blows up at those values. fokkerplanck2_complete should work now.

--- compute_boundary_condition.m
+++ compute_boundary_condition.m
@@ -10,6 +10,7 @@ p = pde.params;
 L = xMax-xMin;
 Tol_Cel_Num = 2^(Lev);
 h = L  / Tol_Cel_Num;
+small_dx = h*1e-7; %Parameter to fix problem with Inhomogeneous BC
 DoF = Deg * Tol_Cel_Num;
 
 bc = sparse(DoF,1);
@@ -22,14 +23,32 @@ if strcmp(LorR,'L')
     WorkCel = 0;
     c = [1:Deg];
     IntVal =  p_L'*Fun(xMin,p,t) ;
-    bc(c) = - g_func(xMin,p,t).*IntVal;
+    if isfinite(g_func(xMin,p,t)) %make sure g_func is finite
+        
+        bc(c) = -g_func(xMin,p,t).*IntVal;
     
+    else
+        
+        xLclose = xMin + small_dx; %Move away from xMin by small_dx
+        bc(c) = -g_func(xLclose,p,t).*IntVal;
+        
+    end
 else
     
     WorkCel = Tol_Cel_Num - 1;
     c = Deg*WorkCel+[1:Deg];
     IntVal =  p_R'*Fun(xMax,p,t);
-    bc(c) = g_func(xMax,p,t).*IntVal;
+    
+    if isfinite(g_func(xMax,p,t)) %make sure g_func is finite
+        
+        bc(c) = g_func(xMax,p,t).*IntVal;
+        
+    else
+        
+        xRclose = xMax - small_dx; %Move away from xMax by small_dx
+        bc(c) = g_func(xRclose,p,t).*Intval;
+        
+    end
     
 end
 
