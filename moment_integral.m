8ef33369bce8eb23c8cda4203f8fa73544673a7c
Louis Wonnell <lwonnell@ksu.edu>
2020-02-10 16:28:17 -0500

    Moved the normalization of Gaussian weights in moment_integral.m to the proper location after the for loop. Function now gives proper answers for test_moment inside asgard.m

--- moment_integral.m
+++ moment_integral.m
@@ -13,8 +13,6 @@ quad_ww = 2^(-Lev(1))/2*quad_ww;
 
 ww = repmat(quad_ww, 2^Lev(1), 1); 
 
-ww = ww.*(xmax - xmin); %for 2D use Kronmult
-
 if num_dimensions >= 2
     for i = 2:num_dimensions
          domainMin = pde.dimensions{1,i}.domainMin;
@@ -23,15 +21,17 @@ if num_dimensions >= 2
     end
 end
 
-[x, w] = lgwt(deg, 0, h);
+ww = ww.*(xmax - xmin);
 
-points = [];
+%[x, w] = lgwt(deg, 0, h);
+
+%points = [];
 %num = 2^Lev*deg;
 
 
-for i = 0:2^Lev-1
-    points = [points; xmin + x + i*h];
-end
+%for i = 0:2^Lev(i)-1
+%    points = [points; xmin + x + i*h];
+%end
 
 %points2 = repmat(points, [num 1]);
 
