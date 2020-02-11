33ebd4e6f9bbfd203990b2d1abe72e829e399501
Tyler McDaniel <bmcdan16@utk.edu>
2020-02-11 13:46:01 -0500

    faster means of computing symmetric pos definite

--- analyze_matrix.m
+++ analyze_matrix.m
@@ -6,37 +6,22 @@ fprintf('\n\nbegin matrix analysis\n');
 % matrices tested are symmetric or positive definite
 
 % 1: test for symmetry
-%is_sym = isequal(A,A');
+is_sym = isequal(A,A');
+
+if is_sym
+    fprintf("--symmetric--\n");
+else
+    fprintf("--nonsymmetric--\n");
+end
 
 
-%if is_sym
-%    fprintf("--symmetric--\n");
-%else
-%    fprintf("--nonsymmetric--\n");
-%end
+[~,flag] = chol(A, 'lower');
 
-
-
-% 2: test for pos def
-%test = zeros(size(A));
-%if is_sym
-%    test = A;
-%else
-%    test = A + A';
-%end
-
-%pos_def = 0;
-%eigenvals = eig(test);
-%non_positive_eigenvals = (eigenvals <= 0);
-%if sum(non_positive_eigenvals) > 0
-%    pos_def = 1;
-%end
-
-%if pos_def    
-%    fprintf("--positive definite--\n");
-%else
-%    fprintf("--not positive definite--\n");
-%end
+if flag == 0    
+    fprintf("--symmetric positive definite--\n");
+else
+    fprintf("--not symmetric positive definite--\n");
+end
 
  
 % 3: spectrum
@@ -48,10 +33,10 @@ saveas(gcf,'spectrum.png');
 
 
 % 4: condition number of eigenvect mat
-fprintf('eigen condition number: %d\n', rcond(eigenvectors));
+fprintf('eigen condition number: %d\n', cond(eigenvectors));
 
 % 5: condition number of matrix
-fprintf('matrix condition number: %d\n', rcond(A));
+fprintf('matrix condition number: %d\n', cond(A));
 
 % 6: sparsity %
 fprintf('matrix density: %f\n', nnz(A)/numel(A));
