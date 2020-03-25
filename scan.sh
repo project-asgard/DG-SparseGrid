1c8501d3450c8fa09af0467d13e65fe7a560e5f0
Louis Wonnell <8zw@fusiont5.ornl.gov>
2020-03-25 15:13:48 -0400

    2D Complete Data at lev=3 for p_max = 7

--- scan.sh
+++ scan.sh
@@ -2,7 +2,7 @@
 MATLAB=/usr/local/bin/matlab
 for lev in {3..3}
 do
-for deg in {3..8}
+for deg in {3..7}
 do
 $MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'lev',$lev,'deg',$deg,'implicit',true, 'dt', 0.1, 'num_steps', 8, 'grid_type', 'FG', 'time_independent_A', true);" &
 done
