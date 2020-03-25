ba41975934e41e1104f5690c361372e887dfe9bd
Louis Wonnell <8zw@fusiont5.ornl.gov>
2020-03-25 19:14:48 -0400

    Lev = 3 dt = 0.05 results for 2D Complete

--- scan.sh
+++ scan.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MATLAB=/usr/local/bin/matlab
-for lev in {5..5}
+for lev in {3..3}
 do
 for deg in {3..7}
 do
-$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'lev',$lev,'deg',$deg,'implicit',true, 'dt', 0.1, 'num_steps', 8, 'grid_type', 'FG', 'time_independent_A', true);" &
+$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'lev',$lev,'deg',$deg,'implicit',true, 'dt', 0.05, 'num_steps', 16, 'grid_type', 'FG', 'time_independent_A', true);" &
 done
 done
