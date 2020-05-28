f32352143de4262e1597c5d45f63755f07bcc9eb
lwonnell <lwonnell@ksu.edu>
2020-05-28 17:53:24 -0400

    plot_fval modified to get appropriate Sparse Grid plots. Scan.sh modified to run full 2D case quickly

--- scan.sh
+++ scan.sh
@@ -4,6 +4,6 @@ for lev in {3..3}
 do
 for deg in {1..4}
 do
-$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(continuity1,'lev',$lev,'deg',$deg,'implicit',false);" &
+$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method', 'BE','dt', 1, 'num_steps', 50,'grid_type', 'SG','lev',$lev,'deg',$deg, 'time_independent_A', true);" &
 done
 done
