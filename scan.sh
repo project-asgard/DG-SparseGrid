23f8b25fe446b3f986451307fe5573d38ab073a6
Louis Wonnell <8zw@fusiont5.ornl.gov>
2020-05-29 11:35:45 -0400

    Output from SG data is added and scan.sh file was modified to run SG simulations

--- scan.sh
+++ scan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-MATLAB=/Applications/MATLAB_R2019a.app/bin/matlab
-for lev in {3..3}
+MATLAB=/usr/local/bin/matlab
+for lev in {3..5}
 do
-for deg in {1..4}
+for deg in {3..7}
 do
 $MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method', 'BE','dt', 1, 'num_steps', 50,'grid_type', 'SG','lev',$lev,'deg',$deg, 'time_independent_A', true);" &
 done
