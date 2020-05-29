373ba5c8a1ed7ca1da07b34e44fb4e231b739543
Louis Wonnell <8zw@fusiont5.ornl.gov>
2020-05-29 15:13:41 -0400

    Modified plot_fval to label outputs with level and degree specified for f1d_x

--- plot_fval.m
+++ plot_fval.m
@@ -79,7 +79,7 @@ if nDims==2
     x = nodes{1};
     y = nodes{2};
     ax1 = subplot(2,2,1);
-    fName = ['output/f1d_x-' sprintf('deg_%d',deg1) '.mat'];
+    fName = ['output/f1d_x-' sprintf('deg_%d_lev_%d',deg1,lev1) '.mat'];
     save(fName, 'x', 'f1d');
 %    plot(x,f1d,'-o');
     semilogy(x,f1d,'LineWidth', 2); %semilog for SG output
