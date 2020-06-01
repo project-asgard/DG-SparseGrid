85251ceda9dcc5e2193534694851045df3465c60
lwonnell <lwonnell@ksu.edu>
2020-06-01 13:02:53 -0400

    Changed scan.sh to run FG simulation with time_independent_A turned off

--- scan.sh
+++ scan.sh
@@ -4,6 +4,6 @@ for lev in {5..5}
 do
 for deg in {6..7}
 do
-$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method', 'BE','dt', 1, 'num_steps', 50,'grid_type', 'SG','lev',$lev,'deg',$deg, 'time_independent_A', true);" &
+$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method', 'BE','dt', 1, 'num_steps', 50,'grid_type', 'FG','lev',$lev,'deg',$deg);" &
 done
 done
