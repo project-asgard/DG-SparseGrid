df91f43fc47a58d52e6fa3d9b2f51fcb332b8ead
lwonnell <lwonnell@ksu.edu>
2020-06-03 15:47:11 -0400

    Added script for plotting SG simulations and modified scan.sh to do time convergence

--- scan.sh
+++ scan.sh
@@ -2,8 +2,8 @@
 MATLAB=/usr/local/bin/matlab
 for lev in {5..5}
 do
-for deg in {3..7}
+for deg in {6..7}
 do
-$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method', 'BE','dt', 0.05, 'num_steps', 600,'grid_type', 'SG','lev',$lev,'deg',$deg);" &
+$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method', 'BE','dt', 0.125, 'num_steps', 400,'grid_type', 'SG','lev',$lev,'deg',$deg);" &
 done
 done
