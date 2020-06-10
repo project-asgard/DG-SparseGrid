f0b5e20aa54da6d0eb71e79117aff0ebdd6f4724
David Green <greendl1@ornl.gov>
2020-06-10 09:04:37 -0400

    improved scan.sh

--- scan.sh
+++ scan.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 MATLAB=matlab
+
+grid_type=FG
+dt=1
+num_steps=50
+PDE=fokkerplanck2_complete
+timestep_method=BE
+
 for lev in {5..6}
 do
-for deg in {4..9}
+for deg in {5..9}
 do
-$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method','BE','lev',$lev,'deg',$deg,'dt',1,'num_steps',50,'grid_type','FG','time_independent_build_A',true,'quiet',false,'save_output',true,'output_filename_id','-l$lev-d$deg-FG','plot_freq',100,'save_freq',100);" &
+
+ID=-$PDE-l$lev-d$deg-$grid_type-$dt-$num_steps-$timestep_method
+
+$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard($PDE,'timestep_method','$timestep_method','lev',$lev,'deg',$deg,'dt',$dt,'num_steps',$num_steps,'grid_type','$grid_type','time_independent_build_A',true,'quiet',false,'save_output',true,'output_filename_id','$ID','plot_freq',100,'save_freq',100);" > log-asgard$ID.txt 2>&1 &
+
 done
 done
