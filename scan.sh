e04fda4c9c867222d9faf869ae1dfb3735dcad6f
David Green <greendl1@ornl.gov>
2020-06-10 08:42:56 -0400

    updates to scan.sh

--- scan.sh
+++ scan.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-MATLAB=/Applications/MATLAB_R2020a.app/bin/matlab
-for lev in {4..5}
+MATLAB=matlab
+for lev in {5..6}
 do
-for deg in {4..8}
+for deg in {4..9}
 do
-$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method','BE','lev',$lev,'deg',$deg,'dt',1,'num_steps',50,'grid_type','SG','time_independent_build_A',true,'quiet',false,'save_output',true,'output_filename_id','-l$lev-d$deg')
-;" &
+$MATLAB -noFigureWindows -batch "addpath(genpath('./'));asgard(fokkerplanck2_complete,'timestep_method','BE','lev',$lev,'deg',$deg,'dt',1,'num_steps',50,'grid_type','FG','time_independent_build_A',true,'quiet',false,'save_output',true,'output_filename_id','-l$lev-d$deg-FG','plot_freq',100,'save_freq',100);" &
 done
 done
