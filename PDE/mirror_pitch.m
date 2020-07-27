68cdee71462506027b3221747c5cd74e8f0911c0
lwonnell <lwonnell@ksu.edu>
2020-07-27 09:40:11 -0400

    mirror_pitch.m modified to work in pitch angle coordinate system, in domain (-pi, pi). Initial tests show it behaves normally

--- PDE/mirror_pitch.m
+++ PDE/mirror_pitch.m
@@ -6,10 +6,10 @@ function pde = mirror_pitch
 % distribution that has a Maxwellian form. 
 % PDE:
 % 
-% df/dt == nu_D d/dz ( (1-z^2) df/dz )
+% df/dt == nu_D/(2*sin(z)) d/dz ( sin(z) df/dz )
 %
 % nu_D is the deflection frequency
-% Domain is [-1,1]
+% Domain is [-pi,pi]
 % Homogeneous Neumann boundary condition 
 % Code will be added to equation involving velocity dimension
 %
@@ -22,10 +22,9 @@ function pde = mirror_pitch
 % asgard(mirror_pitch);
 %
 % implicit
-% asgard(mirror_pitch,'timestep_method','CN');
+% asgard(mirror_pitch,'timestep_method','CN', 'deg', 3, 'lev', 3);
 %
-%Louis: With small nu, stability is maintained for longer times
-% asgard(diffusion1,'lev',3,'deg',4,'timestep_method','BE', 'dt',0.05,'num_steps',20)
+
 pde.CFL = 0.01;
 
 %% Setup the dimensions
@@ -60,17 +59,17 @@ nu_D = 10^9; %deflection frequency in s^-1 for v approximately equatl to v_b
 n_o = 0.5*n_b; %initial number density for specie at specific velocity
 v_o = 0.5*v_b; %initial known velocity
 
-function y = dd1(n)
+%function y = dd1(n)
 % Our default value is 0
-y = 0; 
+%y = 0; 
 
 % The function is 1 only if the input is 0
-if n == 0
-    y = 1;
-end
+%if n == 0
+%    y = 1;
+%end
 
-end
-soln_v = @(x) x.*0 + n_o.*(m_a/(2*pi*T_b))^3/2.*exp(-m_a.*(x-v_o).^2/(2*T_b));
+%end
+soln_z = @(z) n_b.*cos(z);
 soln_t = @(t) t.*0 + 1;
 
 %BCFunc = @(x) soln_x(x);
@@ -90,10 +89,10 @@ soln_t = @(t) t.*0 + 1;
 %    @(t,p) BCFunc_t(t)
 %    };
 
-dim_x.name = 'v_par';
-dim_x.domainMin = 0.001;
-dim_x.domainMax = v_b;
-dim_x.init_cond_fn = @(x,p,t) soln_v(x)*soln_t(t);
+dim_z.name = 'z';
+dim_z.domainMin = -pi;
+dim_z.domainMax = pi;
+dim_z.init_cond_fn = @(z,p,t) soln_z(z)*soln_t(t);
 
 
 %%
@@ -101,7 +100,7 @@ dim_x.init_cond_fn = @(x,p,t) soln_v(x)*soln_t(t);
 % Note that the order of the dimensions must be consistent with this across
 % the remainder of this PDE.
 
-pde.dimensions = {dim_x};
+pde.dimensions = {dim_z};
 num_dims = numel(pde.dimensions);
 
 %% Setup the terms of the PDE
@@ -109,15 +108,22 @@ num_dims = numel(pde.dimensions);
 % Here we have 1 term1, having only nDims=1 (x) operators.
 
 %% 
-% termC
+% termC == nu_D/(2*sin(z))*d/dz sin(z)*df/dz
 %
-% d/dz( (1-z^2) df/dz )
+% becomes 
+%
+% termC == g1(z) q(z)        [mass, g1(p) = nu_D/(2*sin(z)),  BC N/A]
+%   q(p) == d/dz g2(z) r(z)   [grad, g2(p) = sin(z), BCL=D,BCR=N]
+%   r(p) == d/dp g3(z) f(z)   [grad, g3(p) = 1,      BCL=N,BCR=D]
 
-g1 = @(z,p,t,dat) 1-z.^2;
-g2 = @(z,p,t,dat) z.*0+1;
-pterm1  = GRAD(num_dims,g1,-1,'D','D');
+
+g1 = @(z,p,t,dat) nu_D./(2*sin(z));
+g2 = @(z,p,t,dat) sin(z);
+g3 = @(z,p,t,dat) z.*0 + 1;
+pterm1  = MASS(g1);
 pterm2  = GRAD(num_dims,g2,+1,'N','N');
-termC_z = TERM_1D({pterm1,pterm2});
+pterm3 = GRAD(num_dims,g3,-1,'D','D');
+termC_z = TERM_1D({pterm1,pterm2,pterm3});
 termC   = TERM_ND(num_dims,{termC_z});
 
 %%
@@ -146,7 +152,7 @@ pde.sources = {};
 % This requires nDims+time function handles.
 
 pde.analytic_solutions_1D = { ...
-    @(x,p,t) soln_v(x), ...
+    @(z,p,t) soln_z(z), ...
     @(t,p) soln_t(t) 
     };
 
