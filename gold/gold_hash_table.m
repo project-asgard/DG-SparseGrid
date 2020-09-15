25c122bb8f9f55de190273323838abb00fe4aded
Tyler McDaniel <bmcdan16@utk.edu>
2020-09-15 12:35:28 -0400

    complete rebase

--- gold/gold_hash_table.m
+++ gold/gold_hash_table.m
@@ -2,49 +2,13 @@
 
 addpath(genpath(pwd));
 
-
-
 % element_table testing files
 data_dir = strcat("generated-inputs", "/", "element_table", "/");
 root = get_root_folder();
 [stat,msg] = mkdir ([root,'/gold/',char(data_dir)]);
 
-out_format = strcat(data_dir, "element_table_1_2_SG.dat");
-num_dimensions = 1;
-lev_vec = zeros(num_dimensions,1) + 2;
-grid_type = 'SG';
-[fwd1, inv1] = hash_table_nD(lev_vec, grid_type);
-inv1_mat = cell2mat(inv1);
-inv1_mat = reshape(inv1_mat,[3*num_dimensions,size(inv1,2)])';
-coord = inv1_mat(:,1:2*num_dimensions);
-filename = out_format;
-write_octave_like_output(filename,coord);
-
-out_format = strcat(data_dir, "element_table_2_3_SG.dat");
-num_dimensions = 2;
-lev_vec = zeros(num_dimensions,1) + 3;
-grid_type = 'SG';
-[fwd2, inv2] = hash_table_nD(lev_vec, grid_type);
-inv2_mat = cell2mat(inv2);
-inv2_mat = reshape(inv2_mat,[3*num_dimensions,size(inv2,2)])';
-coord = inv2_mat(:,1:2*num_dimensions);
-filename = out_format;
-write_octave_like_output(filename,coord);
-
-out_format = strcat(data_dir, "element_table_3_4_FG.dat");
-num_dimensions = 3;
-lev_vec = zeros(num_dimensions,1) + 4;
-grid_type = 'FG';
-[fwd3, inv3] = hash_table_nD(lev_vec, grid_type);
-inv3_mat = cell2mat(inv3);
-inv3_mat = reshape(inv3_mat,[3*num_dimensions,size(inv3,2)])';
-coord = inv3_mat(:,1:2*num_dimensions);
-filename = out_format;
-write_octave_like_output(filename,coord);
-
-%FIXME delete above
 % element table tests
-
+levels = {};
 levels{1} = [7];
 levels{2} = [5, 2];
 levels{3} = [3, 2, 3];
@@ -53,7 +17,6 @@ out_format = strcat(data_dir, "table_%dd_%s.dat");
 id_out_format = strcat(data_dir, "ids_%dd_%s.dat");
 
 for i=1:size(levels, 2)
-    
     [sparse_elements, sparse_elementsIDX] = hash_table_sparse_nD (levels{i}, max_adapt_lev, 'SG');
     [level_row,level_col,level_val] = find(sparse_elements.lev_p1);
     [cell_row, cell_col, cell_val] = find(sparse_elements.pos_p1);
