045a6a664e78175e8db0b7ef4a13c1d1038a5061
Tyler McDaniel <bmcdan16@utk.edu>
2020-09-24 12:00:48 -0400

    update dense FMWT block generation

--- gold/gold_basis.m
+++ gold/gold_basis.m
@@ -5,11 +5,11 @@ root = get_root_folder();
 [stat,msg] = mkdir ([root,'/gold/',char(data_dir)]);
 
 out_format = strcat(data_dir, 'transform_blocks_l%i_d%i_%d.dat');
-levels = 12;
-for i=1:levels
+levels = [2 5];
+for i = levels
     for d=2:3:5
         [FWMT, blocks] = OperatorTwoScale_wavelet2(d, i);
-        for b=1:i+1
+        for b = 1:length(blocks)
             write_octave_like_output(sprintf(out_format,i,d,b), blocks{b});
         end
     end
