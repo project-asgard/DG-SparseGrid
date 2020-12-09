7b512d94d03a43686dfdad57ee0d462441b9ef1b
Tyler McDaniel <bmcdan16@utk.edu>
2020-12-09 15:26:35 -0500

    complete gold generation for adaptivity

--- gold/gold_adapt.m
+++ gold/gold_adapt.m
@@ -1,4 +1,4 @@
-% continuity1 sg l4d3
+% adaptivity functions
 data_dir = "generated-inputs/adapt/";
 out_format = strcat(data_dir, 'continuity1_l4_d3_');
 run_adapt(@continuity1,@fval_gen_1, out_format, ...
@@ -11,6 +11,14 @@ run_adapt(@continuity3,@fval_gen_2, out_format, ...
     'lev',4,'deg',4,'grid_type','SG');
 
 
+% initial condition adapt
+out_string = strcat(data_dir, 'diffusion2_l2_d3_initial.dat');
+run_initial(@diffusion2,out_string, ...
+     'lev',2,'deg',3,'grid_type','SG');
+out_string = strcat(data_dir, 'diffusion1_l3_d4_initial.dat');
+run_initial(@diffusion1,out_string, ...
+     'lev',3,'deg',4,'grid_type','SG');  
+ 
 function [x] = fval_gen_1(num_entries, elem_size)
     x = zeros(num_entries, 1);
     num_elems = num_entries/elem_size;
@@ -105,4 +113,48 @@ function run_adapt(pde_handle, fval_gen, out_format, varargin)
          full(hash_table.elements.pos_p1(id,:))) - 1;
   end
   write_octave_like_output(strcat(out_format,'refine_table.dat'),table);
+end
+
+
+function run_initial(pde_handle, out_string, varargin)
+  opts = OPTS(varargin);  
+  opts.quiet = 1;
+  opts.adapt_threshold = 1e-4;
+  opts.max_lev_coeffs = 1;
+
+  pde = pde_handle( opts );
+  t = 0;
+  TD = 0;
+  pde = get_coeff_mats(pde,opts,t,TD,opts.use_oldcoeffmat);
+
+  lev_vec = get_lev_vec(pde);
+
+  [elements, elements_idx]    = hash_table_sparse_nD (lev_vec, ...
+                                          opts.max_lev, opts.grid_type);
+  hash_table.elements         = elements;
+  hash_table.elements_idx     = elements_idx;
+  
+  fval = initial_condition_vector(pde, opts, hash_table, t);
+
+  keep_adapting_initial_condition = true;
+  while keep_adapting_initial_condition
+      num_pre_adapt = numel(fval);
+      % first refine
+      [pde,fval_tmp,hash_table,~,~,~,~,~,~,~,~,~] ...
+                = adapt(pde,opts,[],fval,hash_table,[],[],[],[], ...
+                [],0,1);
+      if num_pre_adapt == numel(fval_tmp)
+          keep_adapting_initial_condition = false;
+      end
+      clear fval_tmp;
+      % reproject initial condition onto refined basis
+      fval = initial_condition_vector(pde, opts, hash_table, t);
+  end
+  % coarsen
+  [pde,~,hash_table,~,~,~,~,~,~,~,~,~] ...
+    = adapt(pde,opts,[],fval,hash_table,[],[],[],[], ...
+      [],1,0);
+  % reproject onto coarsend basis
+  fval = initial_condition_vector(pde, opts, hash_table, t);
+  write_octave_like_output(out_string,fval);
 end
