befdb54cef1d9545f7f589793966f5ffac907c6a
lwonnell <lwonnell@ksu.edu>
2021-01-05 11:05:21 -0500

    Cleaned up mirror files to make variables more informative. Modified case 3 inside mirror3 to have initial condition of beam in physical space that can be modified.

--- 2020-10-mirror_3D_scripts/plot_mirror_output.m
+++ 2020-10-mirror_3D_scripts/plot_mirror_output.m
@@ -1,24 +1,24 @@
 function plot_mirror_output(nodes, f_nD, f_nD_analytic, pde)
 
-    x = nodes{1};
-    x = 0.5*9.109*10^-31*x.^2/(1.602*10^-19);
-    f1d = f_nD{1,2};
-    plot(x,f1d,'-o');
-% %    y = nodes{2};
-% %    z = nodes{3};
+     x = nodes{1};
+%     x = 0.5*9.109*10^-31*x.^2/(1.602*10^-19);
+%     f1d = f_nD{1,2};
+%     plot(x,f1d,'-o');
+    y = nodes{2};
+    z = nodes{3};
+    
+    nx = numel(x);
+    ny = numel(y);
+    nz = numel(z);
 %     
-%     nx = numel(x);
-% %    ny = numel(y);
-% %    nz = numel(z);
-%     
-%     %%
-%     % Plot a 1D line through the solution
-%     
-%     sz = numel(f_nD(:,1,1))/2;
-%     sy = numel(f_nD(1,:,1))/2;
-%     sx = numel(f_nD(1,1,:)) - 1;
-%     
-%     %plotting 
+    %%
+    % Plot a 1D line through the solution
+    
+    sz = numel(f_nD{1,6}(:,1,1))/2;
+    sy = numel(f_nD{1,6}(1,:,1))/2;
+    sx = 5;%numel(f_nD{1,6}(1,1,:))/2;
+    
+    %plotting 
 %     f1d = f_nD(:,sy,sx);
 %     f1d = f1d(1,:);
 %     x = nodes{1};
@@ -38,10 +38,10 @@ function plot_mirror_output(nodes, f_nD, f_nD_analytic, pde)
 % %     if pde.checkAnalytic
 % %         f_1d_analytic = f_nD_analytic(:,sy,sx);
 % %         f_1d_analytic = f_1d_analytic(1,:);
-          hold on;
-          f_1d_analytic = f_nD_analytic{1,2};
-          plot(x,f_1d_analytic,'-');
-          hold off;
+%           hold on;
+%           f_1d_analytic = f_nD_analytic{1,2};
+%           plot(x,f_1d_analytic,'-');
+%           hold off;
 % %     end
 %     
 %     sx = max(1,floor(nx/2));
@@ -60,12 +60,14 @@ function plot_mirror_output(nodes, f_nD, f_nD_analytic, pde)
 %     %%
 %     % Plot 2D
 %     
-%     ax1 = subplot(2,2,3);
-%     f_nD_with_noise = f_nD;
-%     f_nD_with_noise(1,1) = f_nD_with_noise(1,1)*1.0001;
-%     contourf(x,y,f_nD_with_noise,'LineColor','none');
-%     title('numeric 2D solution');
-%     
+    ax1 = subplot(2,3,1);
+    sx = 8;
+    contourf(z,y,f_nD{1,6}(:,:,sx)');
+    ax2 = subplot(2,3,2);
+    title('2D slice through 3D numeric');
+    sx = 24;
+    contourf(z,y,f_nD{1,6}(:,:,sx)');
+
 
 
 end
--- pdes/mirror_parameters.m
+++ pdes/mirror_parameters.m
@@ -9,36 +9,36 @@ Lx_offset = 0;                     % [m]
 N = 200;                           % number of elements on profile
 % =========================================================================
 % Axial domain:
-z = linspace(-Lx/2,Lx/2,N)' + Lx_offset;
+%z = linspace(-Lx/2,Lx/2,N)' + Lx_offset;
 
 m_e = 9.109*10^-31; %electron mass in kg
 m_D = 3.3443*10^-27; %deuterium mass in kgs
 m_H = 1.6726*10^-27; %hydrogen mass in kgs
-k_b = 1.380*10^-23; %Boltzmann constant in Joules/Kelvin
+boltz = 1.380*10^-23; %Boltzmann constant in Joules/Kelvin
 e = 1.602*10^-19; %charge in Coulombs
 ln_delt = 10; %Coulomb logarithm
 v_th = @(T_eV,m) sqrt(2*T_eV * e/m);
 eps0 = 8.85*10^-12; %permittivity of free space in Farad/m
 mu0 = 4*pi*10^-7; %magnetic permeability in Henries/m
 n_turns = 10;
-R_mag = 0.6; %radius of individual loop in meters
-I_mag = 10; %current going through individual loop in meters
-B_o = mu0*n_turns*I_mag/(2*R_mag); %magnetic field under current loop
-v_test = 500; %test value for velocity in coefficient
-z_test = pi/2 - 1e-6; %test value for pitch angle in coefficient
-advec_space_1D = v_test*cos(z_test); %advection coefficient for 1D in space
-n  = [+5, +5]*1e3; %number of loops in each coil
-coil_radius  = [R_mag, R_mag]; %radii of coils
-z0 = [-1.5, 1.5]; %location of coils in physical space
-I  = [+1.5,1.5]*1e2; %current going through each coil in amps
+radius_loop = 0.6; %radius of individual loop in meters
+current_loop = 10; %current going through individual loop in meters
+magn_field = mu0*n_turns*current_loop/(2*radius_loop); %magnetic field under current loop
+vel_test = 500; %test value for velocity in coefficient
+pitch_test = pi/2 - 1e-6; %test value for pitch angle in coefficient
+advec_space_1D = vel_test*cos(pitch_test); %advection coefficient for 1D in space
+coil_nloops  = [+5, +5]*1e3; %number of loops in each coil
+coil_radius  = [radius_loop, radius_loop]; %radii of coils
+coil_coords = [-1.5, 1.5]; %location of coils in physical space
+coil_i  = [+1.5,1.5]*1e2; %current going through each coil in amps
 
 % species b: electrons in background
-b.n = 4e19;
-b.E_eV = 0;
-b.T_eV = 4;
-b.Z = -1;
-b.m = m_e;
-b.vth = v_th(b.T_eV,b.m);
+b.n = 4e19; %number density of background species in m^-3
+b.E_eV = 0;%energy of background species
+b.T_eV = 4;%temperature/spread of background species
+b.Z = -1;%atomic number of species
+b.m = m_e; %mass of background
+b.vth = v_th(b.T_eV,b.m);%thermal velocity of background
 
 %species b2: deuterium in background
 b2.n = 4e19;
@@ -50,25 +50,25 @@ b2.vth = v_th(b2.T_eV,b2.m);
 
 % species a: electrons in beam
 a.n = 4e19;
-a.E_eV = 1e3;
-a.T_eV = 50;
+a.E_eV = 1e3; %energy of beam species
+a.T_eV = 100; %temperature/spread of beam species
 a.Z = -1;
-a.z0 = pi/4; %location of beam injection in pitch 
-a.dz0 = a.z0/4;
-a.s0 = 0;
-a.ds0 = 0.1;
+a.z0 = pi/4; %location of beam injection in pitch angle (radians)
+a.dz0 = a.z0/4; %spread of beam in pitch angle
+a.s0 = 0; %location of beam in space (m)
+a.ds0 = 2.5; %spread of beam in space (m)
 a.m = m_e;
 a.vth = v_th(a.T_eV,a.m);
-a.v_beam = sqrt(2*e.*a.E_eV./a.m);
+a.v_beam = sqrt(2*e.*a.E_eV./a.m); %velocity of beam species
 
-x       = @(v,vth) v./vth; %normalized velocity to thermal velocity
-xi      = @(s,R_mag) s/R_mag; %normalized spatial coordinate to radius of magnetic coil
+vel_norm       = @(v,vth) v./vth; %normalized velocity to thermal velocity
+space_norm      = @(s,R_mag) s/R_mag; %normalized spatial coordinate to radius of magnetic coil
 nu_ab0  = @(a,b) b.n * e^4 * a.Z^2 * b.Z^2 * ln_delt / (2*pi*eps0^2*a.m^2*b.vth^3); %scaling coefficient
-nu_s    = @(v,a,b) nu_ab0(a,b) .* (1+a.m/b.m) .* psi(x(v,b.vth)) ./ x(v,b.vth); %slowing down frequency
-nu_par  = @(v,a,b) nu_ab0(a,b).*(psi(x(v,b.vth))./(x(v,b.vth).^3)); %parallel diffusion frequency
-nu_D    = @(v,a,b) nu_ab0(a,b).*(phi_f(x(v,b.vth)) - psi(x(v,b.vth)))./(x(v,b.vth).^3); %deflection frequency in s^-1
+nu_s    = @(v,a,b) nu_ab0(a,b) .* (1+a.m/b.m) .* psi(vel_norm(v,b.vth)) ./ vel_norm(v,b.vth); %slowing down frequency
+nu_par  = @(v,a,b) nu_ab0(a,b).*(psi(vel_norm(v,b.vth))./(vel_norm(v,b.vth).^3)); %parallel diffusion frequency
+nu_D    = @(v,a,b) nu_ab0(a,b).*(phi_f(vel_norm(v,b.vth)) - psi(vel_norm(v,b.vth)))./(vel_norm(v,b.vth).^3); %deflection frequency in s^-1
 maxwell = @(v,offset,vth) a.n/(pi^3/2.*vth^3).*exp(-((v-offset)/vth).^2);
-gauss   = @(v,x,y) 1/(sqrt(2*pi)*x)*exp(-0.5*((v - x)/y).^2);
+gauss   = @(v,x,y) 1/(sqrt(2*pi)*y)*exp(-0.5*((v - x)/y).^2);
 B_func = @(s) exp(s); % %magnetic field as a function of spatial coordinate
 dB_ds = @(s) exp(s); 
 
@@ -80,10 +80,10 @@ B_func2 = @get_B;
     function B = get_B(s)
         B = s.*0;
         for coil = 1:numel(coil_radius)
-            zz = z0(coil);
+            zz = coil_coords(coil);
             rr = coil_radius(coil);
-            II = I(coil);
-            nn = n(coil);
+            II = coil_i(coil);
+            nn = coil_nloops(coil);
             B = B + (mu0.*nn.*II./(2*rr)).*(1 + ((s-zz)./rr).^2 ).^(-3/2);%magnetic field from sum of loops around mirror
         end
     end
@@ -94,33 +94,33 @@ dB_ds2 = @get_dBds;
         
         dBds = s.*0;
         for coil = 1:numel(coil_radius)
-            zz = z0(coil);
+            zz = coil_coords(coil);
             rr = coil_radius(coil);
-            II = I(coil);
-            nn = n(coil);    
+            II = coil_i(coil);
+            nn = coil_nloops(coil);    
             dBds = dBds -3*(mu0.*nn.*II./(2.*rr)).*(s-zz)./rr./(rr.*(1 + ((s-zz)./rr).^2).^(5/2)); %derivative of field around loops
         end
     end
 
-advec_time_1D = @(t) exp(-2*v_test*cos(z_test)*t);
-uniform = @(x,p,t) x.*0 + 1; %uniform condition if needed
+advec_time_1D = @(t) exp(-2*vel_test*cos(pitch_test)*t);
+uniform = @(x,p,t) x.*0 + 1; %uniiform condition if needed
 
-init_cond_v = @(v,p,t) maxwell(v,a.v_beam,a.vth);
+init_cond_v = @(v,p,t) gauss(v,a.v_beam,a.vth);
 init_cond_z = @(z,p,t) gauss(z,a.z0,a.dz0);
 init_cond_s = @(s,p,t) gauss(s,a.s0,a.ds0);
 init_cond_t = @(t,p) t*0 + 1;
 
 boundary_cond_v = @(v,p,t) maxwell(v,0,b.vth);%exp(-nu_D(v,a,b).*t);
-boundary_cond_z = @(z,p,t) z.*0 + 0.153667;
-boundary_cond_s = @(s,p,t) s.*0 + 1;
+boundary_cond_z = @(z,p,t) z.*0 + 1;
+boundary_cond_s = @(s,p,t) s.*0;
 boundary_cond_t = @(t,p) t.*0 + 1;
 
-s_z = @(z,p,t) -0.5.*(sin(z)+cot(z).*cos(z)); %source pitch fuction for mirror3
-s_v = @(v,p,t) nu_D(v,a,b);
-s_space = @(s,p,t) s.*0 + 1;
-s_time = @(t,p) t.*0 + 1;
+source_pitch = @(z,p,t) -0.5.*(sin(z)+cot(z).*cos(z)); %source pitch fuction for mirror3
+source_vel = @(v,p,t) nu_D(v,a,b);
+source_space = @(s,p,t) s.*0 + 1;
+source_time = @(t,p) t.*0 + 1;
 
-source_3D = {s_z, s_v, s_space, s_time};
+source_3D = {source_pitch, source_vel, source_space, source_time};
 
 
 soln_s = @solution_s;
@@ -129,7 +129,7 @@ soln_s = @solution_s;
     end
 soln_v = @solution_v;
     function ret = solution_v(v,p,t)
-        ret = a.n/(pi^3/2.*v_th(b.E_eV,a.m).^3).*exp(-(v./v_th(b.E_eV,a.m)).^2);
+        ret = a.n/(pi^3/2.*v_th(b.T_eV,a.m).^3).*exp(-(v./v_th(b.T_eV,a.m)).^2);
     end
 soln_z = @solution_z;
     function ret = solution_z(z,p,t)
