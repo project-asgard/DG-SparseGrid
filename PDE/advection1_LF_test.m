5080b7133783c61e8d544c135b6c3b4966e5a11a
David Green <david.lindsay.green@gmail.com>
2021-01-28 16:18:37 -0500

    added advection1_LF pde for testing LF flux

---
+++ PDE/advection1_LF_test.m
@@ -0,0 +1,20 @@
+function tests = advection1_LF_test()
+fh = localfunctions;
+tests = functiontests(fh);
+end
+
+function advection1_LF_left_test(testCase)
+addpath(genpath(pwd));
+disp('Testing the left(-1) flux');
+% setup PDE
+args = {'lev',4,'deg',4,'dt',1e-2,'quiet',true,'num_steps',20,'timestep_method','matrix_exponential'};
+opts = OPTS(args);
+pde = advection1_LF(opts);
+% modify PDE - not needed here
+% run PDE
+[err,fval,fval_realspace,nodes,err_realspace] = asgard_run_pde(opts,pde);
+% assert on correctness
+rel_err = err / norm(fval);
+verifyLessThan(testCase,rel_err,1e-3);
+end
+
