9db45fe2831a2528c0363745e9100db023025ce9
lwonnell <wonnelllb@ornl.gov>
2021-05-04 02:21:47 -0400

    Added mirror_Mfunctional.m to PDE and mirror_Mfunctional_test.m to general folder. This tests the M functional from Franz for CQL4D.

---
+++ PDE/mirror_Mfunctional.m
@@ -0,0 +1,25 @@
+function ans = mirror_Mfunctional(wfunc,umin,lindex)
+
+% evaluation of M_l (Franz, 1987) using input function w(u) for value of
+% index value lindex
+% output is a 1D function in u-space
+
+% M_l(w) = int^{u_max}_u w(u') (u'/gamma')^{1-l} gamma'^2 du'
+
+%speed of light
+
+c = 3*10^8; %m/s
+
+%setting up integrand matrix
+
+gamma = @(u) sqrt(1 + u.^2./c^2); %relativistic correction
+
+M_int = @(u) gamma(u).^2.*(u./gamma(u)).^(1 - lindex);
+
+%M_int = reshape(M_int,size(wfunc));
+%gamma = reshape(gamma, size(wfunc));
+integrand = @(u) wfunc(u).*M_int(u);
+%integrating
+ans = integral(integrand, umin, Inf);
+
+end
