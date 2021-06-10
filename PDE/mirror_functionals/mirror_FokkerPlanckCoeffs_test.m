1883107e38a3eceed8a85dd76ea331a8c8fb449a
lwonnell <wonnelllb@ornl.gov>
2021-06-10 18:39:36 -0400

    Modified code within mirror_FokkerPlanckCoeffs.m to be more readable. mirror_FokkerPlanckCoeffs_test.m is producing values, which need to be verified.

--- PDE/mirror_functionals/mirror_FokkerPlanckCoeffs_test.m
+++ PDE/mirror_functionals/mirror_FokkerPlanckCoeffs_test.m
@@ -7,7 +7,7 @@ end
 
 function test_FokkerPlanckCoeffs_Mawellian(testCase)
 
-uVals = [3.5314*10^6, 7.3150*10^6, 1.1099*10^7, 1.4882*10^7, 1.8666*10^7, 2.2450*10^7, 2.6233*10^7, 3.0017*10^7, 3.3800*10^7, 3.7584*10^7];; %test velocity in m/s
+uVals = [3.5314*10^6, 7.3150*10^6, 1.1099*10^7, 1.4882*10^7, 1.8666*10^7, 2.2450*10^7, 2.6233*10^7, 3.0017*10^7, 3.3800*10^7, 3.7584*10^7]; %test velocity in m/s
 temp = 100; %temperature in eV
 n_o = 8e20; %equilibrium density in eV
 e = 1.602*10^-19; %charge in Coulombs
@@ -15,14 +15,16 @@ m_e = 9.109*10^-31; %electron mass in kg
 u_th = sqrt(2*temp*e/m_e); %thermal velocity in m/s
 rel_tol = 1e-6;
 func = @(u,z) n_o.*exp(-u.^2./u_th^2).*(z.*0 + 1)./(pi^(3/2)*u_th^3);
-testVal = [];
+test_Avals = [];
+testVals = [];
 
 %testing Rosenbluth Coeffs for l = 0
 lIndex = 0;
 z = pi/4;
 gold_M = 2.9798*10^-3;
 for i = 1:length(uVals)
-    testVals = mirror_FokkerPlanckCoeffs(func,uVals(i),lIndex,z);
+    uVal = uVals(i);
+    testVals = mirror_FokkerPlanckCoeffs(func,uVal,lIndex,z);
     test_Avals(i) = testVals(1);
 end
 rel_err = abs(testVal(1) - gold_M)/abs(gold_M);
