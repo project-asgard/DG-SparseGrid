b9a02cdaa97c5e2c626c171c714bd3624aab45b7
lwonnell <wonnelllb@ornl.gov>
2021-07-27 15:16:23 -0400

    Added the capability to do multiple background species for mirror1_collision_div.m and mirror2_collision_div.m, and mirror_FokkerPlanckCoeffs_test.m.

--- PDE/mirror2_collision_div.m
+++ PDE/mirror2_collision_div.m
@@ -37,20 +37,30 @@ function pde = mirror2_collision_div(opts)
 % eq2 :      q == C(v) * grad(f)  [pterm2: grad(g(v)=C(v),-1, BCL=N, BCR=N]
 % Run with
 %
-% asgard(@mirror2_collision_div,'timestep_method','matrix_exponential','case',3, 'lev', 4, 'deg', 3,'num_steps', 60, 'dt',5e-5, 'normalize_by_mass', true, 'calculate_mass', true)
+% asgard(@mirror2_collision_div,'timestep_method','BE','case',3, 'lev', 4, 'deg', 3,'num_steps', 60, 'dt',5e-5, 'normalize_by_mass', true, 'calculate_mass', true)
 
 params = mirror_parameters();
 
 switch opts.case_
     case 1 
-        params.a.T_eV = 0.05*params.b.T_eV; %Target temperature in Kelvin\
-        offset = 10^6; %case with offset and change in Temperature
+        params.a.T_eV = 50;
+        params.b.T_eV = params.a.T_eV;
+        params.b2.T_eV = params.a.T_eV;
+        params.a.vth = params.v_th(params.a.T_eV,params.a.m);
+        params.b.vth = params.v_th(params.b.T_eV,params.b.m);
+        params.b2.vth = params.v_th(params.b2.T_eV,params.b2.m);
+        params.a.v_beam = 0; %case with no beam velocity
+        params.init_cond_v = @(v,p,t) params.soln_v(v,p,t);
     case 2 
-        params.a.T_eV = 0.05*params.b.T_eV;
-        offset = 0; %case with no offset but change in Temperature
+%         params.a.T_eV = 5*params.b.T_eV;
+%         params.a.vth = params.v_th(params.a.T_eV,params.a.m);
+%         params.a.v_beam = 0;
+        params.init_cond_v = @(v,p,t) params.maxwell(v,0,3e5);
     case 3 
-        params.a.T_eV = 10;
+        params.a.T_eV = 50;
+        params.a.vth = params.v_th(params.a.T_eV,params.a.m);
         params.a.E_eV = 3e3; %case with offset and no change in Temperature
+        params.init_cond_v = @(v,p,t) params.maxwell(v,params.a.v_beam,params.a.vth);
 end
 maxwell = @(v,x,y) a.n/(pi^3/2.*y^3).*exp(-((v-x)/y).^2);
 
@@ -68,7 +78,6 @@ dimensions = {dim_v, dim_th};
 num_dims = numel(dimensions);
 
 %% Define the analytic solution (optional)
-
 soln1 = new_md_func(num_dims,{ ...    
     params.soln_v, ...
     params.soln_z, ...
@@ -100,49 +109,66 @@ BCR = new_md_func(num_dims,{...
 % eq1 :  df/dt == div(A(v) * q)        [pterm1: div (g(v)=A(v),+1, BCL=?, BCR=?)]
 % eq2 :      q == A(v) * grad(f)       [pterm2: grad(g(v)=A(v),-1, BCL=D, BCR=N)]
 
-A = @(v,p) sqrt(0.5.*v.^2.*(p.nu_par(v,p.a,p.b) + p.nu_par(v,p.a,p.b2)));
+dV_v = @(x,p,t,d) x.^2; 
+dV_th = @(x,p,t,d) sin(x);
+
+g1 = @(v,p,t,dat) v.*0 + 1;
+pterm1 = MASS(g1,'','',dV_th);
+term1_th = SD_TERM({pterm1,pterm1});
+
+A = @(v,p) v.*sqrt(0.5*(p.nu_par(v,p.a,p.b) + p.nu_par(v,p.a,p.b2)));
 g1 = @(v,p,t,dat) A(v,p);
 g2 = @(v,p,t,dat) A(v,p);
 
 pterm1 = DIV (num_dims,g1,'',+1,'D','N','','','',dV_v);
 pterm2 = GRAD(num_dims,g2,'',-1,'N','D','','','',dV_v);
 term1_v = SD_TERM({pterm1,pterm2});
-term1   = MD_TERM(num_dims,{term1_v,[]});
+term1   = MD_TERM(num_dims,{term1_v,term1_th});
 
 % term2 is a div using B(v) = v (m_a/(m_a + m_b))nu_s
 %
 % eq1 :  df/dt == div(B(v) * f)       [pterm1: div(g(p)=B(v),+1, BCL=?, BCR=?]
 
-B = @(v,p) v*p.a.m.*(p.nu_s(v,p.a,p.b)./(p.a.m + p.b.m) + p.nu_s(v,p.a,p.b2)./(p.a.m + p.b2.m));
-g3 = @(v,p,t,dat) B(v,p);
+dV_v = @(x,p,t,d) x.^2; 
+dV_th = @(x,p,t,d) sin(x);
 
-pterm1 = DIV(num_dims,g3,'',-1,'N','D','','','',dV_v);
+A = @(v,p) v.*0 + 1;
+g1 = @(v,p,t,dat) A(v,p);
+
+pterm1 = MASS(g1,'','',dV_th);
+
+term2_th = SD_TERM({pterm1});
+
+B = @(v,p) v.*p.a.m.*(p.nu_s(v,p.a,p.b)./(p.a.m + p.b.m) + p.nu_s(v,p.a,p.b2)./(p.a.m + p.b2.m));
+g2 = @(v,p,t,dat) B(v,p);
+
+pterm2 = DIV(num_dims,g2,'',-1,'N','D','','','',dV_v);
+
+term2_v = SD_TERM({pterm2});
+term2   = MD_TERM(num_dims,{term2_v,term2_th});
 
 % term3 is done using SLDG defining C(v) = sqrt(nu_D(v)/2)
 %
 % eq1 :  df/dt == div( q)   [pterm1: div (g(v)=C(v),+1, BCL=D, BCR=D]
 % eq2 :      q ==  grad(f)  [pterm2: grad(g(v)=C(v),-1, BCL=N, BCR=N]
 
-term2_v = SD_TERM({pterm1});
-term2   = MD_TERM(num_dims,{term2_v,[]});
 
 dV_v = @(x,p,t,d) x; %changing for MASS term
 dV_th = @(x,p,t,d) sin(x);
 
-C = @(v,p) sqrt((p.nu_D(v,p.a,p.b) + p.nu_D(v,p.a,p.b2))/2);
+C = @(v,p) v.*sqrt((p.nu_D(v,p.a,p.b) + p.nu_D(v,p.a,p.b2))/2);
 g4 = @(v,p,t,dat) C(v,p);
-pterm1 = MASS(g4,[],[],dV_v);
+pterm1 = MASS(g4,'','',dV_v);
 term3_v = SD_TERM({pterm1,pterm1});
 
 D = @(v,p) v.*0 + 1;
 g5 = @(v,p,t,dat) D(v,p);
-pterm1 = DIV (num_dims,g5,'',+1,'D','N','','','',dV_th);
-pterm2 = GRAD(num_dims,g5,'',-1,'N','D','','','',dV_th);
+pterm1 = DIV (num_dims,g5,'',+1,'D','D','','','',dV_th);
+pterm2 = GRAD(num_dims,g5,'',-1,'N','N','','','',dV_th);
 term3_th = SD_TERM({pterm1,pterm2});
 term3   = MD_TERM(num_dims,{term3_v,term3_th});
 
 terms = {term1,term2,term3};
-
 
 %% Define sources 
 
