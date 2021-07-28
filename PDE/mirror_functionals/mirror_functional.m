3eb476f8f64ee395e144c9a207a303cfbe9740b2
lwonnell <wonnelllb@ornl.gov>
2021-07-28 19:39:23 -0400

    Testing out trapz versus integral inside mirror_Ucoeff.m and diagnosing U_coeff inside mirror_FokkerPlanckCoeffs.m

--- PDE/mirror_functionals/mirror_functional.m
+++ PDE/mirror_functionals/mirror_functional.m
@@ -34,12 +34,12 @@ integrand_R = @(u) wfunc(u).*R_int(u);
 integrand_E = @(u) wfunc(u).*E_int(u);
 
 %integrating
- M = integral(integrand_M, uLimit, 100*uLimit,'RelTol',0,'AbsTol',1e-12);
+% M = integral(integrand_M, uLimit, 100*uLimit,'RelTol',0,'AbsTol',1e-12);
 % N = integral(integrand_N, 0, uLimit,'RelTol',0,'AbsTol',1e-12);
 % R = integral(integrand_R, uLimit, 100*uLimit,'RelTol',0,'AbsTol',1e-12);
 % E = integral(integrand_E, 0, uLimit,'RelTol',0,'AbsTol',1e-12);
 
-%M = trapz(u_high,integrand_M(u_high));
+M = trapz(u_high,integrand_M(u_high));
 N = trapz(u_low,integrand_N(u_low));
 R = trapz(u_high,integrand_R(u_high));
 E = trapz(u_low,integrand_E(u_low));
